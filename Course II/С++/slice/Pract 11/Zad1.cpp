17b0b8a8e9dcb5705ab94911b95db43f24aec6a2
Georgiy Demenchuk <demenchuk.george@mail.ru>
2016-12-08 19:33:49 +0300

    Pract 11

---
+++ Course II/С++/slice/Pract 11/Zad1.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>;
+#include <iostream>;
+#include <math.h>;
+using namespace std;
+int main(){
+
+  int x,y;
+  long double c;
+  printf("x="); scanf("%d",&x);
+  printf("y="); scanf("%d",&y);
+  c=log10((y-sqrt(log10(x)))*(x-(y/(x+pow(x,2)/4))));
+  printf("Result: => %d",c);
+	 
+}
---
+++ Course II/С++/slice/Pract 11/Zad4.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>;
+#include <iostream>;
+using namespace std;
+bool tochka(int t1, int t2){
+    return ((t1 <= -2) && (t1 >= -6) && (t2 >2) &&(t2 < 7)) || ((t1 <= -4) &&(t1 >= -6)&&(t2 >= -3)&&(t2 <= 2)) ? true : false
+;}
+
+int main(){
+
+  int x,y;
+    bool trigger;
+
+  printf("x => "); scanf("%d",&x);
+  printf("y => "); scanf("%d",&y);
+
+  trigger = tochka(x,y);
+    (trigger == 1) ? cout<<"True" : cout<<"False";
+}
