126bb64e67f5a468a16c29f0df3996020db0bb31
Georgiy Demenchuk <demenchuk.george@mail.ru>
2016-12-10 21:43:31 +0300

    Looks better

--- Course II/С++/slice/Pract 13/Zad2.cpp
+++ Course II/С++/slice/Pract 13/Zad2.cpp
@@ -1,32 +1,35 @@
 ﻿#include <iostream>
-
 using namespace std;
-int NOD(int a, int b,int nod){
-    if (a%b==0) {nod=b;}
-           else{
-                int r=b;
-                b=a%b;   
-                a=r;
-                nod=b;
-           }
-     return nod;
+
+int nod(int a, int b,int n){
+    if (a%b==0){
+      n=b;
     }
-    
+    else{
+      int r=b;
+      b=a%b;
+      a=r;
+      n=b;
+    }
+  return n;
+}
+
 int main(){
-       int chislo1, chislo2;
-       cout << "Введите два числа для нахождения НОД =>" << endl;
-       cin >> chislo1;
-       cin >> chislo2;
-       int nod=0;
-       if (chislo1 < chislo2){
-           int p=chislo2; chislo2=chislo1; chislo1=p;}
-       do{   
-           nod = NOD(chislo1,chislo2,nod);
-          } while (chislo1%chislo2!=0);
-       if (nod!=1) {
-                   cout << "НОД => " << nod << endl;
-       }
-       else {
-                   cout << "Числа не имеют НОД" << endl;
-       }  
+    int c1, c2;
+    cout<<"Введите два числа для нахождения НОД =>" << endl;
+    cin>>c1;
+    cin>>c2;
+    int n=0;
+    if (c1 < c2){
+      int p=c2; c2=c1; c1=p;}
+    do{
+      n = nod(c1,c2,n);
+    } while (c1%c2!=0);
+
+    if (n!=1){
+      cout << "НОД => " << n << endl;
+    }
+    else{
+      cout << "Числа не имеют НОД" << endl;
+    }
 }
