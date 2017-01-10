cc6692bcb67111627a77b54e6d47e54138a51f4d
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-01-10 23:30:32 +0300

    Pract 19 base description added

--- Course II/С++/pract/Pract 19/practics.cpp
+++ Course II/С++/pract/Pract 19/practics.cpp
@@ -16,6 +16,34 @@ bool validation(int s1, int s2, int s3) {
     return (s1==s2) || (s1==s3) || (s2==s3) ? true : false;
 }
 
+//Функция точки для n11_4
+bool tochka(int t1, int t2){
+    return ((t1 <= -2) && (t1 >= -6) && (t2 >2) &&(t2 < 7)) || ((t1 <= -4) &&(t1 >= -6)&&(t2 >= -3)&&(t2 <= 2)) ? true : false
+;}
+
+//Строктура для n13_1
+struct point
+ {
+  char name[10];
+  int x;
+  int y;
+ };
+
+//Функция НОД для n13_2
+int nod(int a, int b,int n){
+    if (a%b==0){
+      n=b;
+    }
+    else{
+      int r=b;
+      b=a%b;
+      a=r;
+      n=b;
+    }
+  return n;
+}
+
+//Далее номера практических и работ
 int n11_1(){
 
   int x,y;
@@ -59,6 +87,20 @@ int n11_3(){
     (trigger == 1) ? cout<<"True" : cout<<"False";
 
 return 0; 
+}
+
+int n11_4(){
+
+  int x,y;
+    bool trigger;
+
+  printf("x => "); scanf("%d",&x);
+  printf("y => "); scanf("%d",&y);
+
+  trigger = tochka(x,y);
+    (trigger == 1) ? cout<<"True" : cout<<"False";
+
+return 0;
 }
 
 int n12_1(){
@@ -133,5 +175,72 @@ int n12_2(){
   cout<<"\nСумма ряда: "<<setprecision(5)<<-0.5*sum; 
 
 cout<<"\n";
+return 0;
+}
+
+int n13_1(){    
+    setlocale(LC_ALL, "rus");
+
+  int i, j, ind1, ind2;
+  double s, d;
+  int num_points;
+  double x,c,sum,y;
+  int n,q; 
+  sum=0;
+  n=1;
+  
+  initx:  
+      cout<<"x => ";cin>>x;
+  
+  if ((x*x)>(doublepi/4)){
+      cout<<"\nПовторите ввод\n";
+      goto initx;
+  } 
+  
+  c=sin(x)*sin(x); 
+  sum=c; 
+  y=log(cos(x));
+  
+  cout<<"Значение ln cos(x) = "<<setprecision(5)<<y; 
+  cout<<"N слагаемое S(x)"; 
+  
+  do{ 
+      cout<<"\n"<<n; 
+      c=c*(n*sin(x)*sin(x))/(n+1);
+      sum+=c; 
+      cout<<setw(12)<<setprecision(5)<<c; 
+      cout<<setw(12)<<setprecision(5)<<-0.5*sum; 
+      n++;
+  } 
+  while ((fabs(c)>=e)&&(n<1000)); 
+  cout<<"\n\nЗаданная точность достигается за "<<n-1<<" шага"; 
+  cout<<"\nСумма ряда: "<<setprecision(5)<<-0.5*sum; 
+
+cout<<"\n";
+return 0;
+}
+
+int n13_2(){
+
+    int c1, c2;
+
+    cout<<"Введите два числа для нахождения НОД =>";
+    cin>>c1;
+    cin>>c2;
+
+    int n=0;
+    if (c1 < c2){
+      int p=c2; c2=c1; c1=p;}
+    do{
+      n = nod(c1,c2,n);
+    } while (c1%c2!=0);
+
+    if (n!=1){
+      cout << "НОД => " << n << endl;
+    }
+    else{
+      cout << "Числа не имеют НОД" << endl;
+    }
+
 return 0;
 }
