bdfd65f2426152be6744b0685b92ab1448760717
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-02-20 17:23:26 +0300

    УП Pract 2 style fix

--- Course II/С++/УП/Pract 2/Zad2.cpp
+++ Course II/С++/УП/Pract 2/Zad2.cpp
@@ -9,33 +9,31 @@ using namespace std;
 int main(){
 	int a,b,c;
 	double result;
-	cout<<"\n";
-	cout<<"Введите число А => "; cin>>a;
+
+	cout<<"\nВведите число А => "; cin>>a;
 	cout<<"Введите число B => "; cin>>b;
 	cout<<"Введите число C => "; cin>>c;
 
 	if ((a==b) && (b==c) && (c==a)){
 		cout<<"Все числа равны!\n";
 		result=a*a+b*b+c*c;
-		cout<<"Сумма квадратов всех чисел: "<<result;
+		cout<<"Сумма квадратов всех чисел: "<<result<<"\n";
+		exit(0);
 	}
 	else if ((a>b) && (b>c)){
 		b=a;
 		c=a;
-		cout<<"\na="<<a<<"\nb="<<b<<"\nc="<<c;
 	}
 	else if ((b>a) && (c<b)){
 		a=b;
 		c=b;
-		cout<<"\na="<<a<<"\nb="<<b<<"\nc="<<c;
 	}
 	else if ((c>a) && (c>b)){
 		a=c;
 		b=c;
-		cout<<"\na="<<a<<"\nb="<<b<<"\nc="<<c;
 	}
 
-cout<<"\n";
+cout<<"\na="<<a<<"\nb="<<b<<"\nc="<<c<<"\n";
 return 0;
 
 }                                                 
