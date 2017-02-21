7260dbc0f97acd191d36426a74c922ed04f9b955
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-02-21 14:24:30 +0300

    Assembly Pract 2

---
+++ Course II/Assembly/Pract 2/Zad1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+#include <iomanip>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+	int i,x;
+    float y;
+
+    for (i=1;i<31;i++){
+        x=i;
+        if (x>=16)
+            y=(x-11)*(x-11)-125;
+        
+        if (x<16)
+            y= (float) (x*x+72*x-6400)/168;
+
+        cout<<i<<". "<<y<<"\n";
+    }
+
+cout<<"\n";
+return 0;
+}
