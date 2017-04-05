3f0dd810cc463e4cfb7b875d0e6f536638997d62
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-04-05 15:47:20 +0300

    УП Pract 12 Zad1 fixed

--- Course II/С++/УП/Pract 12/Zad1.cpp
+++ Course II/С++/УП/Pract 12/Zad1.cpp
@@ -3,18 +3,23 @@
  
 using namespace std;
 
-int x;
- 
-int RSum(int N)
-{
-    if(N==0) return 0;
-    cout<<N<<"\n";
-    return pow(x,N)+RSum(pow(x,N)-1);
+int x, n;
+
+int pow(int a,int n){
+  return (!n)?1:a*pow(a,n-1);
+}
+
+int counter(int x,int a){
+    if (a <= 0)
+        return 0;
+    else if (a)
+        return pow(x,a) + counter(x,a-1);
 }
 
 int main()
 {   
-	x=2;
-    RSum(2);
+	cout<<"Введите x => "; cin>>x;
+	cout<<"Введите n => "; cin>>n;
+	cout<<"S = "<<counter(x,n)<<"\n";
     return 0;
 }
