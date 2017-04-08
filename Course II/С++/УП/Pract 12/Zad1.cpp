b36ee6c3f8d66e19c792bdd6838d3af59a0f0e46
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-04-08 21:54:25 +0300

    УП Pract 12 fixed

--- Course II/С++/УП/Pract 12/Zad1.cpp
+++ Course II/С++/УП/Pract 12/Zad1.cpp
@@ -3,23 +3,39 @@
  
 using namespace std;
 
-int x, n;
+int x, n, k;
 
-int pow(int a,int n){
-  return (!n)?1:a*pow(a,n-1);
+void init(){
+	cout<<"Введите x => "; cin>>x;
+	cout<<"Введите n => "; cin>>n;
 }
 
 int counter(int x,int a){
     if (a <= 0)
         return 0;
-    else if (a)
+    else
         return pow(x,a) + counter(x,a-1);
 }
 
-int main()
-{   
-	cout<<"Введите x => "; cin>>x;
-	cout<<"Введите n => "; cin>>n;
-	cout<<"S = "<<counter(x,n)<<"\n";
-    return 0;
+int main(){
+	do
+	  {
+	    cout<<"\n\n1. Ввод x и a\n2. Вывод суммы последовательности\n0. Выход из программы\n=> ";
+	    cin>>k;
+	    switch (k)  
+	    {  
+	      case 1:  
+	        init();
+	        break;
+	        
+	      case 2:  
+	        cout<<"S = "<<counter(x,n)<<"\n";
+	        break;
+	    }    
+
+	  }
+	  while (k!=0);
+
+cout<<"\n";
+return 0;
 }
