bb7a2d9d86ba86302dcc9199f80a95a4b4a7fa31
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-05-01 00:29:37 +0300

    СП Pract 6 && Pract 7 DOCX added

--- Course II/СП/Pract 7/Zad1.cpp
+++ Course II/СП/Pract 7/Zad1.cpp
@@ -2,71 +2,76 @@
 #include "iostream"
 using namespace std;
 
-unsigned short x, y, z, s1, s2, s3;
+unsigned short x = 3;
+unsigned short y = 4;
+unsigned short z = 14;
+unsigned short result, s1, s2, s3;
 
 int main() {
+
 	setlocale(LC_ALL, "rus");
-	cout << "Введите x =>"; cin >> x;
-	cout << "Введите y =>"; cin >> y;
-	cout << "Введите z =>"; cin >> z;
+
 	_asm {
 
 		mov ax, x
 		mov bx, y
-		mov cx, z 
+		mov cx, z
 		add bx, ax
 		mov s1, bx
 		cmp ax, bx
-		jb Down
-		ja Up
-		je Ravno
-		
-		Down :
+		jb DOWN
+		ja UP
+		je RAVNO
+
+		DOWN :
 			mov dx, bx
 			jmp KON
-		
-		Up :
+
+		UP :
 			mov dx, ax
 			jmp KON
-		
-		Ravno :
+
+		RAVNO :
 			mov dx, bx
 			jmp KON
-		
+
 		KON :
 			mov ax, y
 			add ax, y
 			add ax, y
 			mov bx, z
 			cmp ax, bx
-			jb Down1
-			ja Up1
-			je Ravno1
-		
-		Down1 :
+			jb DOWN1
+			ja UP1
+			je RAVNO1
+
+		DOWN1 :
 			mov cx, ax
 			jmp KON1
-		
-		Up1 :
+
+		UP1 :
 			mov cx, bx
 			jmp KON1
-		
-		Ravno1 :
+
+		RAVNO1 :
 			mov cx, ax
 			jmp KON1
-		
+
 		KON1 :
 			mov s2, dx
 			mov s3, cx
 			add dx, cx
-			mov z, dx
+			mov result, dx
 	}
 
-	printf("\nBX = %i", s1);
+	printf("x = %i", x);
+	printf("\ny = %i", y);
+	printf("\nz = %i", z);
+	printf("\n\nBX = %i", s1);
 	printf("\nDX (1-я часть) = %i", s2);
-	printf("\nCX (2-я часть) = =%i", s3);
-	printf("\nРезультат: %i\n\n", z);
+	printf("\nCX (2-я часть) = %i", s3);
+	printf("\n\nРезультат = %i\n", result);
 	system("pause");
-	
+
 return 0;
 }
