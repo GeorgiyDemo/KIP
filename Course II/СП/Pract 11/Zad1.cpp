c31443f75799c61d96813a1e5f2d7f51de5db43f
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-05-30 21:27:23 +0300

    СП Pract 11 codefixed

--- Course II/СП/Pract 11/Zad1.cpp
+++ Course II/СП/Pract 11/Zad1.cpp
@@ -2,31 +2,43 @@
 #include <iomanip>
 #include <conio.h>
 
-
-char kol=0,k = 18;
+char  k = 15;
+char result;
 
 void main() {
 
 	setlocale(LC_ALL, "rus");
+	printf("Входные данные       Выходные данные");
 
-	_asm {
+	for (k = 1; k < 19; k++) {
+		result = 0;
+		_asm {
 
-		mov bh, k
-
-		mov cx, 9
-		m1: mov dx, cx
+			mov al, 0
 			mov cx, 9
-			m2 : mov ax, dx
-	
-				 add ax, cx
-				 cmp ax, bh
-				 je e1
-				 jmp e2
-				 e1 : add kol,1
-					  e2 : loop m2
-						   mov cx, dx
-						   jcxz m1
+			mov dh, 1
+
+		null:
+			mov bx, cx
+			mov cx, 10
+			mov dl, 0
+		one :
+			mov ah, dh
+			add ah, dl
+			cmp ah, k
+			jne two
+			inc al
+
+		two :
+			inc dl
+			loop one
+			mov cx, bx
+			inc dh
+			loop null
+			mov result, al
+		
+		}
+		printf("\nk = %d                result = %d", k, result);
 	}
-	printf("%d", kol);
 	_getch();
 }
