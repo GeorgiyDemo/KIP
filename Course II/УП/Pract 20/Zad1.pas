d8ff375f4df3f0d484ff736a27e17e5191c934c9
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-06-07 16:24:09 +0300

    УП Pract 20 changed

--- Course II/УП/Pract 20/Zad1.pas
+++ Course II/УП/Pract 20/Zad1.pas
@@ -3,11 +3,12 @@
 //б) обе функции в одной области экрана
 
 //y:=x*power(sin(1/x),2);
-
 program graph;
 uses graphABC;
-var i,y:real; var x,b:integer;
+  var y,i:real; 
+  var x,b:integer;
 begin
+
     moveto(50,0);
     lineto(50,100);
     moveto(0,50);
@@ -20,51 +21,54 @@ begin
     lineto(150,300);
     moveto(100,250);
     lineto(200,250);
-    x:=-5;
-    y:=x*power(-sin(1/x),2);
+    x:=1;
+    //y:=-i*power(sin(1/i),2);
     b:=round(y);
     moveto(3*x+50,6*b+50);
     
-    for x:=1 to 50 do
-    begin
+    for x:=1 to 50 do begin
         i:=x/10;
-        y:=i*power(-sin(1/i),2);
+        y:=-i*power(sin(1/i),2);
         b:=round(y);
         setpencolor(clred);
         lineto(3*round(i)+50,6*b+50);
     end;
     
-   { x:=-5;
-    y:=-cos(x-1)-abs(x);
+    x:=1;
+    y:=-x*power(sin(1/x),2);
     b:=round(y);
     moveto(3*x+150,6*b+250);
     
-    for x:=-5 to 5 do begin
-        y:=-cos(x-1)-abs(x); b:=round(y);
+   for x:=1 to 50 do begin
+        i:=x/10;
+        y:=-x*power(sin(1/i),2);
+        b:=round(y);
         setpencolor(clred);
-        lineto(3*x+150,6*b+250);
+        lineto(3*round(i)+150,6*b+250);
     end;
     
-    x:=-5;
-    y:=-sin(x)+abs(x+1);
+    x:=1;
+    y:=(x+1)*power(sin(1/x),2);
     b:=round(y);
     moveto(3*x+250,6*b+50);
     
-    for x:=-5 to 5 do begin
-        y:=-sin(x)+abs(x+1); b:=round(y);
+    for x:=1 to 50 do begin
+        y:=(x+1)*power(sin(1/x),2);
+        b:=round(y);
         setpencolor(clblue);
         lineto(3*x+250,6*b+50);
+
     end;
     
-    x:=-5;
-    y:=-sin(x)+abs(x+1);
+    x:=1;
+    y:=(x+1)*power(sin(1/x),2);
     b:=round(y);
     moveto(3*x+150,6*b+250);
     
-    for x:=-5 to 5 do begin
-        y:=-sin(x)+abs(x+1); b:=round(y);
+    for x:=1 to 50 do begin
+        y:=(x+1)*power(sin(1/x),2);
+        b:=round(y);
         setpencolor(clblue);
-        lineto(3*x+150,6*b+250);
+        lineto(3*round(i)+150,6*b+250);
     end;
-    }
 end.
