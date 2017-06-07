e95e34adce7df54c6be1f9f10d6fea697999410d
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-06-07 22:13:59 +0300

    УП Pract 20 final code added

--- Course II/УП/Pract 20/Zad1.pas
+++ Course II/УП/Pract 20/Zad1.pas
@@ -1,74 +1,81 @@
-﻿//ПРИМЕР 1.Построить графики функций y1(x)=cos(x-1)+abs(x), y2(x)=sin(x)-abs(x+1) на отрезке [-5; 5]
-//a) каждую функцию в отдельной области экрана
-//б) обе функции в одной области экрана
+﻿program graph;
+uses graphABC; 
 
-//y:=x*power(sin(1/x),2);
-program graph;
-uses graphABC;
-  var y,i:real; 
-  var x,b:integer;
-begin
+var y,x : real;
+    b,h : integer; 
 
-    moveto(50,0);
-    lineto(50,100);
-    moveto(0,50);
-    lineto(100,50);
-    moveto(250,0);
-    lineto(250,100);
-    moveto(200,50);
-    lineto(300,50);
-    moveto(150,200);
-    lineto(150,300);
-    moveto(100,250);
-    lineto(200,250);
-    x:=1;
-    //y:=-i*power(sin(1/i),2);
-    b:=round(y);
-    moveto(3*x+50,6*b+50);
+begin 
+    moveto(50,0); 
+    lineto(50,100); 
+    moveto(0,50); 
+    lineto(100,50); 
+    moveto(250,0); 
+    lineto(250,100); 
+    moveto(200,50); 
+    lineto(300,50); 
+    moveto(150,200); 
+    lineto(150,300); 
+    moveto(100,250); 
+    lineto(200,250); 
     
-    for x:=1 to 50 do begin
-        i:=x/10;
-        y:=-i*power(sin(1/i),2);
-        b:=round(y);
-        setpencolor(clred);
-        lineto(3*round(i)+50,6*b+50);
-    end;
-    
-    x:=1;
+    x:=0.1; 
     y:=-x*power(sin(1/x),2);
-    b:=round(y);
-    moveto(3*x+150,6*b+250);
+    b:=round(y); 
+    h:=round(x); 
+    moveto(10*h+50,20*b+50); 
     
-   for x:=1 to 50 do begin
-        i:=x/10;
-        y:=-x*power(sin(1/i),2);
-        b:=round(y);
-        setpencolor(clred);
-        lineto(3*round(i)+150,6*b+250);
-    end;
+    repeat 
+        x:=x+0.1; 
+        y:=-x*power(sin(1/x),2);
+        b:=round(y); 
+        h:=round(x); 
+        setpencolor(clred); 
+        lineto(10*h+50,20*b+50); 
+    until(x>10); 
     
-    x:=1;
+    x:=0.1; 
+    y:=-x*power(sin(1/x),2);
+    b:=round(y); 
+    h:=round(x); 
+    moveto(10*h+150,20*b+250); 
+    
+    repeat 
+        x:=x+0.1; 
+        y:=-x*power(sin(1/x),2);
+        b:=round(y); 
+        h:=round(x); 
+        setpencolor(clred); 
+        lineto(10*h+150,20*b+250); 
+    until(x>10); 
+    
+    x:=0.1;
     y:=(x+1)*power(sin(1/x),2);
-    b:=round(y);
-    moveto(3*x+250,6*b+50);
+    b:=round(y); 
+    h:=round(x); 
+    moveto(10*h+250,20*b+50); 
     
-    for x:=1 to 50 do begin
-        y:=(x+1)*power(sin(1/x),2);
-        b:=round(y);
-        setpencolor(clblue);
-        lineto(3*x+250,6*b+50);
-
-    end;
+    repeat 
+        x:=x+0.1;
+        y:=(x+1)*power(sin(1/x),2); 
+        b:=round(y); 
+        h:=round(x); 
+        setpencolor(clblue); 
+        lineto(10*h+250,20*b+50); 
+    until(x>10); 
     
-    x:=1;
+    x:=0.1; 
     y:=(x+1)*power(sin(1/x),2);
-    b:=round(y);
-    moveto(3*x+150,6*b+250);
+    b:=round(y); 
+    h:=round(x); 
+    moveto(10*h+150,20*b+250); 
     
-    for x:=1 to 50 do begin
+    repeat 
+        x:=x+0.1;
         y:=(x+1)*power(sin(1/x),2);
-        b:=round(y);
-        setpencolor(clblue);
-        lineto(3*round(i)+150,6*b+250);
-    end;
+        b:=round(y); 
+        h:=round(x); 
+        setpencolor(clblue); 
+        lineto(10*h+150,20*b+250); 
+    until(x>10); 
+    
 end.
