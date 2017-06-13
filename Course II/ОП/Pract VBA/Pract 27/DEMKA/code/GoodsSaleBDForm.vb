a0c44fe8ae7dd4b5fc18756f8aab906545117e7c
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-06-13 19:40:35 +0300

    ОП Pract 27 PrintButton_Click changed && translater function added

--- Course II/ОП/Pract VBA/Pract 27/DEMKA/code/GoodsSaleBDForm.vb
+++ Course II/ОП/Pract VBA/Pract 27/DEMKA/code/GoodsSaleBDForm.vb
@@ -1,16 +1,166 @@
 Public cn As ADODB.Connection
 Public cli, mngr, sale, content, goods As ADODB.Recordset
 
-Private Sub Frame3_Click()
+Function Translater(number)
 
+    Dim rub, units, dozens, hundrunits, nadc, sizes, i, m
+    
+    If (number >= 1E+15) Or (number < 0) Then
+        Exit Function
+    End If
+    
+    hundrunits = Array("", "сто ", "двести ", "триста ", "четыреста ", "пятьсот ", "шестьсот ", "семьсот ", "восемьсот ", 
+"девятьсот ")
+    dozens = Array("", "", "двадцать ", "тридцать ", "сорок ", "пятьдесят ", "шестьдесят ", "семьдесят ", "восемьдесят ", 
+"девяносто ")
+    nadc = Array("десять ", "одиннадцать ", "двенадцать ", "тринадцать ", "четырнадцать ", "пятнадцать ", "шестнадцать ", 
+"семнадцать ", "восемнадцать ", "девятнадцать ")
+    units = Array("", "один ", "два ", "три ", "четыре ", "пять ", "шесть ", "семь ", "восемь ", "девять ", "", "одна ", "две ")
+    sizes = Array("триллион ", "триллиона ", "триллионов ", "миллиард ", "миллиарда ", "миллиардов ", "миллион ", "миллиона ", 
+"миллионов ", "тысяча ", "тысячи ", "тысяч ", "рубль ", "рубля ", "рублей ")
+
+    rub = Left(Format(number, "000000000000000"), 15)
+
+    If (CDbl(rub) = 0) Then
+        m = "ноль "
+    End If
+    
+    For i = 1 To Len(rub) Step 3
+        If Mid(rub, i, 3) <> "000" Or i = Len(rub) - 2 Then
+                m = m & hundrunits(CInt(Mid(rub, i, 1))) & IIf(Mid(rub, i + 1, 1) = "1", nadc(CInt(Mid(rub, i + 2, 1))), _
+                dozens(CInt(Mid(rub, i + 1, 1))) & units(CInt(Mid(rub, i + 2, 1)) + IIf(i = Len(rub) - 5 And CInt(Mid(rub, i + 
+2, 1)) < 3, 10, 0))) & _
+                IIf(Mid(rub, i + 1, 1) = "1" Or (Mid(rub, i + 2, 1) + 9) Mod 10 >= 4, sizes(i + 1), IIf(Mid(rub, i + 2, 1) = 
+"1", sizes(i - 1), sizes(i)))
+        End If
+    Next i
+    Translater = LCase(Left(m, 1)) & Mid(m, 2)
+    
+End Function
+
+Private Sub CMDFindFirstSearch_Click()
+    Dim skiprecord As Long
+    Dim direction As Long
+    Lname = TextBoxSearch.Text
+    
+    k = InputBox("По какому критерию вы хотите выполнить поиск?" + Chr(13) + "1. Код" + Chr(13) + "2. Дата")
+    
+    Select Case k
+        Case 1
+            criteria = "[№ продажи] = '" & Lname & "'"
+        Case 2
+            criteria = "[Дата продажи] = '" & Lname & "'"
+    End Select
+        
+    skiprecord = 0
+    direction = adSearchForward
+    sale.MoveFirst
+    sale.Find criteria, skiprecord, direction
+             
+    If sale.EOF Then
+        MsgBox "Запись не найдена"
+        Beep
+    Else
+        MsgBox "Запись найдена!"
+        ShowRecord
+        Beep
+    End If
 End Sub
 
-Private Sub ListBox1_Click()
-
-End Sub
 
 Private Sub PrintButton_Click()
+    Dim meow As Integer
+    Dim GoodsDOC As Document
+    Set GoodsDOC = Application.Documents.Add("C:\Users\georgiydemo\Documents\DEMKA\GoodsPrint.docx")
+    meow = 0
+    
+    Selection.MoveDown Unit:=wdLine, Count:=16
 
+    content.MoveFirst
+    While Not content.EOF
+    
+        If (sale.Fields("№ продажи").Value = content.Fields("№ Продажи").Value) Then
+            goods.MoveFirst
+            While Not goods.EOF
+                If (content.Fields("КодТовара").Value = goods.Fields("КодТовара").Value) Then
+                    meow = meow + 1
+                End If
+                goods.MoveNext
+            Wend
+        End If
+        content.MoveNext
+    Wend
+  
+    content.MoveFirst
+    
+        GoodsDOC.Tables.Add Range:=Selection.Range, NumRows:=meow + 1, NumColumns:=4, 
+DefaultTableBehavior:=wdWord9TableBehavior, AutoFitBehavior:=wdAutoFitContent
+            
+            With Selection.Tables(1)
+                If .Style <> "Сетка таблицы" Then
+                    .Style = "Сетка таблицы"
+                End If
+                .Range.Font.Size = 16
+                .Range.Font.Bold = wdToggle
+                .ApplyStyleHeadingRows = True
+                .ApplyStyleLastRow = True
+                .ApplyStyleFirstColumn = True
+                .ApplyStyleLastColumn = True
+               
+                meow = 1
+            content.MoveFirst
+            End With
+        
+    content.MoveFirst
+    While Not content.EOF
+    
+        If (sale.Fields("№ продажи").Value = content.Fields("№ Продажи").Value) Then
+            goods.MoveFirst
+            While Not goods.EOF
+
+                If (content.Fields("КодТовара").Value = goods.Fields("КодТовара").Value) Then
+                    meow = meow + 1
+                    With Selection.Tables(1)
+                        .Cell(meow, 1).Range.Text = Str(meow - 1)
+                        .Cell(meow, 2).Range.Text = goods.Fields("Товар").Value
+                        .Cell(meow, 3).Range.Text = content.Fields("Кол-во").Value
+                        .Cell(meow, 4).Range.Text = content.Fields("Цена").Value
+                
+                    End With
+                End If
+                goods.MoveNext
+            Wend
+        End If
+        content.MoveNext
+    Wend
+    
+    'Хотфикс заголовков таблицы
+    Selection.TypeText Text:="№"
+    Selection.MoveRight Unit:=wdCharacter, Count:=1
+    Selection.TypeText Text:="Название"
+    Selection.MoveRight Unit:=wdCharacter, Count:=1
+    Selection.TypeText Text:="Кол-во (шт)"
+    Selection.MoveRight Unit:=wdCharacter, Count:=1
+    Selection.TypeText Text:="Цена (руб)"
+    Selection.MoveDown Unit:=wdLine, Count:=1
+    
+    With GoodsDOC
+        .Bookmarks("номер_чека").Range.Text = CStr(TextBox10.Text)
+        .Bookmarks("время_покупки").Range.Text = CStr(TextBox11.Text)
+        .Bookmarks("код_клиента").Range.Text = CStr(TextBox6.Text)
+        .Bookmarks("фио_клиента").Range.Text = CStr(TextBox8.Text)
+        .Bookmarks("менеджер_код").Range.Text = CStr(TextBox1.Text)
+        .Bookmarks("менеджер_имя").Range.Text = CStr(TextBox2.Text)
+        .Bookmarks("менеджер_фамилия").Range.Text = CStr(TextBox3.Text)
+        .Bookmarks("менеджер_отчество").Range.Text = CStr(TextBox4.Text)
+        .Bookmarks("менеджер_телефон").Range.Text = CStr(TextBox9.Text)
+        .Bookmarks("сумма_общ").Range.Text = CStr(TextBox14.Text) + " руб. (" + Translater(TextBox14.Text) + ")" + Chr(13)
+        .Bookmarks("время").Range.Text = Time
+        .Bookmarks("дата").Range.Text = Date
+    End With
+    
+    GoodsSaleBDForm.Hide
+    GoodsDOC.Activate
 End Sub
 
 Private Sub UserForm_initialize()
