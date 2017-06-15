14f22312009571894f948420f4a1df70a25befae
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-06-15 22:15:43 +0300

    СП Pract 14 Zad1 added

--- Course II/СП/Pract 14/Zad1.cpp
+++ Course II/СП/Pract 14/Zad1.cpp
@@ -1,98 +1,26 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
+#include <stdlib.h> 
+#include <stdio.h> 
 
-int const n=10;
-bool flag=false;
-int i, k1, menu, sum, *a;
+int main() { 
 
-//Генерация массивов
-void generate(){
+unsigned short a[6]; 
 
-  srand((unsigned)time(0));
-  for (i=0;i<n;i++)
-    a[i]=rand()%20-10;
+unsigned short sum = 0; 
 
-}
+for (short i = 0; i < 6; i++) a[i] = i; 
 
-//Ручный ввод массивов
-void enter(){
+_asm { 
+mov cx, 5 
+mov ebx, 0 
+mov ax, 0 
+cycle: 
+add ebx,2 
+add ax, a[ebx] 
+loop cycle 
+mov sum,ax 
+} 
 
-  cout<<"<Заполняем массив А>\n";
-  for (i=0;i<n;i++){
-     cout<<"Введите элемент ["<<i<<"]: ";
-      cin>>a[i];
-    }
-}
-
-
-//Вывод массивов на экран
-void monitor_out(){
-
-  cout<<"\nМассив А:\n[";
-  for (i=0;i<n;i++){
-    cout<<a[i]<<" ";
-  }
-  cout<<"]";
-}
-
-//Функция элементов массива
-void counter(){
-
-	monitor_out();
-  sum=0;
-	for (i=0;i<n;i++)
-    sum+=a[i];
-  cout<<"\nСумма элементов массива А: "<<sum;
-}
-
-//Меню выбора ввода
-void initarray(){
-
-  a = new int [n];
-
-  cout<<"\n\n1. Генерация массива А\n2. Ручной ввод массива А\n=> ";
-    cin>>k1;
-    switch (k1)
-    {
-      case 1:
-        generate();
-        break;
-
-      case 2:
-        enter();
-        break;
-    }
-
-}
-
-//Главное меню
-int main(){
-
-  do
-  {
-    cout<<"\n\n1. Ввод массива\n2. Вывод массива\n3. Сумма элементов массива\n0. Выход из программы\n=> ";
-    cin>>menu;
-    switch (menu)
-    {
-      case 1:
-        initarray();
-        break;
-
-      case 2:
-        monitor_out();
-        break;
-
-      case 3:
-        counter();
-        break;
-
-    }
-
-  }
-  while (menu!=0);
-
-cout<<"\n";
-return 0;
+printf("%i", sum); 
+system("pause"); 
 
 }
