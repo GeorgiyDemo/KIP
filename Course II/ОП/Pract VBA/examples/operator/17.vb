3358ea73901f758042bdec317904b91f0d08e3c4
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-06-26 23:32:47 +0300

    ОП Pract VBA test task №17 changed

--- Course II/ОП/Pract VBA/examples/operator/17.vb
+++ Course II/ОП/Pract VBA/examples/operator/17.vb
@@ -1,59 +1,58 @@
-'Написать программу на VBA, которая позволяет в прямоугольной матрице 
-осуществлять
-'поиск чисел и их индексы, которые больше по модулю числа, введенного с 
-клавиатуры
-
 Private Sub CommandButton1_Click()
-
-    Dim a(100, 100), i, j, n, m, p, r As Integer
-
-    'Ввод исходных данных
-    Label1.Caption = "Исходная матрица: " + Chr(13)
-    Label2.Caption = "Максимумы строк: " + Chr(13)
-    Label3.Caption = "Преобразованная матрица: " + Chr(13)
-    
-    n = CInt(InputBox("Введите кол-во строк:"))
-    m = CInt(InputBox("Введите кол-во столбцов:"))
-    p = CInt(InputBox("На сколько выполняем сдвиг?"))
-    
-    'Вывод исходной матрицы на Label1
-    For i = 1 To n
-        Label1.Caption = Label1.Caption + Chr(13)
-        For j = 1 To m
-            a(i, j) = CInt(Rnd() * 20 - 10)
-            Label1.Caption = Label1.Caption + CStr(a(i, j)) + Chr(9)
-        Next j
+Dim a(100, 100), n, i, k, ch(100), buf, j, t, h(100), q, w As Integer
+Randomize
+    n = TextBox1
+     w = TextBox2
+      q = TextBox3
+    Label1.Caption = Label1.Caption + "Исходный массив:" + Chr(10)
+    For i = 0 To n - 1
+    Label1.Caption = Label1.Caption + Chr(10)
+    For j = 0 To n - 1
+    a(i, j) = CInt(Rnd() * 100 - 60)
+    Label1.Caption = Label1.Caption + CStr(a(i, j)) + Chr(9)
+    Next j
     Next i
     
-    For r = LBound(a, 2) To UBound(a, 2)
-        If (p < 0) Then
-            For j = 1 To Abs(p)
-                b = a(r, 1)
-                For i = 1 To n - 1
-                    a(r, i) = a(r, i + 1)
-                Next i
-                a(r, n) = b
-            Next j
-        End If
-        
-        If (p >= 0) Then
-            For j = 1 To p
-                b = a(r, n)
-                For i = n To 2 Step -1
-                    a(r, i) = a(r, i - 1)
-                Next i
-                a(r, 1) = b
-             Next j
-        End If
-    Next r
-    
-    For i = 1 To n
-        Label2.Caption = Label2.Caption + Chr(13)
-        For j = 1 To m
-            a(i, j) = CInt(Rnd() * 20 - 10)
-            Label2.Caption = Label2.Caption + CStr(a(i, j)) + Chr(9)
-        Next j
-    Next i
-    
+   If (q = 1) Then
+For k = 1 To w
+  For j = 0 To n - 1
+  i = 0
+  h(j) = a(0, j)
+  Next j
+  For i = 0 To n - 2
+    For j = 0 To n - 1
+      a(i, j) = a(i + 1, j)
+      a(i + 1, j) = a(i, j)
+      Next j
+      Next i
+  For j = 0 To n - 1
+    a(n - 1, j) = h(j)
+    Next j
+Next k
+Else:
+For k = 1 To w
+  For j = 0 To n - 1
+  h(j) = a(n - 1, j)
+  Next j
+  For i = n - 1 To 1 Step -1
+    For j = n - 1 To 0 Step -1
+      a(i, j) = a(i - 1, j)
+      a(i - 1, j) = a(i, j)
+      Next j
+      Next i
+  For j = 0 To n - 1
+    a(0, j) = h(j)
+    Next j
+Next k
+End If
+
+Label3.Caption = Label3.Caption + "Матрица после сдвига:" + Chr(10)
+For i = 0 To n - 1
+Label3.Caption = Label3.Caption + Chr(10)
+For j = 0 To n - 1
+Label3.Caption = Label3.Caption + CStr(a(i, j)) + Chr(9)
+Next j
+Next i
+
 End Sub
 
