c2d8ce0ce64a6dbd41e5544b989cbdf26ed60310
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-06-28 19:11:22 +0300

    ОП Pract VBA test task №19, №23 changed

---
+++ Course II/ОП/Pract VBA/examples/operator/19/19.vb
@@ -0,0 +1,25 @@
+'Написать программу на VBA, которая позволяет в одномерном массиве осуществлять
+'поиск чисел и их индексы, которые меньше по модулю числа, введенного с клавиатуры
+
+Private Sub CommandButton1_Click()
+
+    Dim a(100), KOT, n, i As Integer
+
+    'Ввод исходных данных
+    Label1.Caption = "Исходный массив: " + Chr(13)
+    KOT = CInt(InputBox("Введите число:"))
+    Label2.Caption = "Числа, меньше " + CStr(KOT) + " по модулю:" + Chr(13)
+    n = CInt(InputBox("Введите размерность массива:"))
+
+    'Вывод исходного массива на Label1, вывод чисел меньше по модулю на Label2
+    For i = 1 To n
+        a(i) = CInt(Rnd() * 20 - 10)
+        Label1.Caption = Label1.Caption + CStr(a(i)) + " "
+
+        If (Abs(a(i)) < KOT) Then
+            Label2.Caption = Label2.Caption + CStr(a(i)) + "(" + CStr(i) + ")" + " "
+        End If
+
+    Next i
+
+End Sub
