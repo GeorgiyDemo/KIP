11e6020899662b66627fdf5d6e9c4419b1eb93de
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-09-25 19:45:59 +0300

    ЧМ Pract 3 final fixed

--- Course III/ЧМ/Pract 3/simple.cpp
+++ Course III/ЧМ/Pract 3/simple.cpp
@@ -4,22 +4,8 @@
 
 using namespace std;
 
-const int n=4;
-const int m=4;
-
-int i, j, k, checker, iteration, input;
+int n, m, i, j, k, checker, iteration;
 double **a, *b, *x, *xn, eps;
-
-//Рандомный ввод массивов
-void initarray(){
-    srand((unsigned)time(0)); 
-    for(i=0;i<n;i++){
-        for(j=0;j<m;j++)
-            a[i][j]=rand()%10-5;
-        b[i]=rand()%10-5;
-        x[i]=0;
-    }
-}
 
 //Ручный ввод массивов
 void enter(){
@@ -48,6 +34,7 @@ void outarray(){
             cout<<setw(5)<<a[i][j];
         cout<<setw(5)<<b[i];
     }
+    cout<<"\n";
 }
 
 //Функция метода простых итераций
@@ -59,7 +46,8 @@ void simple_iteration(){
         xn[0] = (b[0] - (a[0][1]*x[1] + a[0][2]*x[2] + a[0][3]*x[3]))/a[0][0]; 
         xn[1] = (b[1] - (a[1][0]*x[0] + a[1][2]*x[2] + a[1][3]*x[3]))/a[1][1];    
         xn[2] = (b[2] - (a[2][0]*x[0] + a[2][1]*x[1] + a[2][3]*x[3]))/a[2][2];
-        xn[3] = (b[3] - (a[3][0]*x[0] + a[3][1]*x[1] + a[3][2]*x[2]))/a[3][3];
+        if (n==4)
+            xn[3] = (b[3] - (a[3][0]*x[0] + a[3][1]*x[1] + a[3][2]*x[2]))/a[3][3];
 
         checker=0;
         for(i=0;i<4;i++)
@@ -71,7 +59,9 @@ void simple_iteration(){
 
         for(int i=0;i<4;i++)
             x[i] = xn[i];
-    cout << "\nИтерация №"<<iteration+1<<":\nx1 = "<<xn[0]  << "\nx2 = " << xn[1] << endl << "x3 = " << xn[2] << endl << "x4 = " << xn[3] << endl;   
+    cout << "\nИтерация №"<<iteration+1<<":\nx1 = "<<xn[0]<<"\nx2 = "<<xn[1]<<"\nx3 = "<<xn[2]<<"\n";
+    if (n==4)
+        cout<<"x4 = " << xn[3]<<"\n"; 
     iteration++;
     }
 
@@ -79,7 +69,7 @@ void simple_iteration(){
             x[i] = xn[i];  
    
     cout << "\n-------------------\nИтераций всего: " << iteration+1;
-    for(i=0;i<4;i++)
+    for(i=0;i<n;i++)
         cout<<"\n"<<x[i];
 
 }
@@ -87,6 +77,8 @@ void simple_iteration(){
 int main(){
 
     cout<<"Введите точность (пример: 0.005) => "; cin>>eps;
+    cout<<"Введите n => "; cin>>n;
+    cout<<"Введите m => "; cin>>m;
 
     a = new double *[m];
     for(i=0;i<n;i++)
@@ -95,20 +87,7 @@ int main(){
     x = new double [m];
     xn = new double [m];
     
-    cout<<"\nКак вы хотите ввести матрицу?\n1. Ручной ввод исходной матрицы\n2. Рандомный ввод исходной матрицы\n=> ";
-    cin>>input;
-
-    switch (input)
-    {
-      case 1:
-        enter();
-        break;
-
-      case 2:
-        initarray();
-        break;
-    }
-
+    enter();
     outarray();
     simple_iteration();
 
