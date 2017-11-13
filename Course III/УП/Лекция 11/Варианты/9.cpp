fd988daa0d82a8489e3550423cbbdab6c9cb0575
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-11-13 20:20:07 +0300

    УП lecture №11 updated

---
+++ Course III/УП/Лекция 11/Варианты/9.cpp
@@ -0,0 +1,35 @@
+//Вариант 9 -> f1 должна взять инкремент x, возвести в квадрат  и сложить с  y  
+#include <iostream>
+using namespace std;
+class MainClass{
+
+    public:
+
+        int x,y;
+        int *p = &x;
+        int *c = &y;
+
+        MainClass(int _x, int _y){
+            *p=_x;
+            *c=_y;
+        }
+
+        MainClass f1(){
+            *p++;
+            *p=*p*(*p);
+            x=x+y;
+            return *this;
+        }
+} ;
+
+int main(){
+    MainClass a(3,4);
+    MainClass *u = &a;
+    u->f1();
+    cout<<"Ответ: "<<u->x;
+    //f1(obj);
+
+cout<<"\n";
+return 0;
+
+};
