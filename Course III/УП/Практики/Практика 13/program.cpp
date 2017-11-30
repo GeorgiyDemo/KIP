ec6a1a9797d8bb9bc0a52bb25e726a95980a26b6
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-11-30 14:49:08 +0300

    УП Pract 13 added

---
+++ Course III/УП/Практики/Практика 13/program.cpp
@@ -0,0 +1,48 @@
+/*
+  Вариант 4
+  Написать программу, которая считывает текст из 
+  файла и выводит на экран только строки,
+  не содержащие двузначных чисел.
+  
+  //Еще добавить потоковый ввод
+*/
+
+#include <cctype>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class StringClass
+{
+
+	protected:
+	    FILE* buf_file;
+	    char str[256];
+
+	public:
+	    void checker();
+};
+
+void StringClass::checker()
+{
+    char cset[] = "1234567890";
+    buf_file = fopen("txt.txt", "r");
+
+    while (!feof(buf_file)) {
+        fgets(str, 256, buf_file);
+        char* symbol = str;
+        while (!isdigit(*symbol))
+            symbol++;
+        int i = strspn(symbol, cset);
+        if (i != 2)
+            puts(str);
+    }
+}
+
+int main()
+{
+    StringClass obj;
+    obj.checker();
+
+    return 0;
+}
