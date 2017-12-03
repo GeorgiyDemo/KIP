8a89cf9ef816877fdea0d80b94fb141e9a0bc75e
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-12-03 17:08:29 +0300

    УП Pract 12 backup

--- Course III/УП/Практики/Практика 12/example.cpp
+++ Course III/УП/Практики/Практика 12/example.cpp
@@ -1,41 +1,62 @@
-
-// Простой пример наследования
+#include <iomanip>
 #include <iostream>
 using namespace std;
-// Определение базового класса
-class В {
-	int i;
+class B{
+ int a;
 public:
-	void set_i(int n) {
-		i = n;
-	};
-	int get_i ( ) ;
 };
-// Определение производного класса
-class D: public В {
-	int j;
-public:
-	void set_j (int n) ;
-	int mul ( ) ;
-};
-// Задание значения i в базовом классе
-// Возвращение значения i в базовом классе
-int В::get_i ()
-{return i;}
-// Задание значения j в производном классе
-void D::set_j (int n)
-{	j = n;}
-// Возвращение значения i базового класса и j — производного
-int D::mul ()
-	// производный класс может вызывать функции-члены базового класса
-{ return j * get_i();
-}
-int main ( )
-{
-	D ob;
-	ob.set_i (10) ; // загрузка i в базовый класс
-	ob.set_j(4); // загрузка j в производный класс
-	cout << ob.mul(); // вывод числа 40
-	return 0;
-}
 
+class D1: public B {
+ int b;
+public:
+};
+
+class D2: private B{
+ int c;
+public:
+};
+
+class D3: private D1, public D2 {
+ int d;
+public:
+};
+
+class D4: public D2, private D1 {
+ int e;
+public:
+};
+//Создадим теперь во всех классах конструкторы, которые смогли бы по цепочке наследования инициализировать свои переменные и передавать остальные значения дальше вверх вплоть до конструктора базового класса. 
+ B(int x) { a=x; }
+
+ D1(int x, int y) : B(y) { b=x;};
+
+ D2(int x, int y) : B(y) { c=x;};
+
+ D3(int x, int y, int z, int i, int j) : D1(y,z), D2(i,j) { d=x;}
+
+ D4(int x, int y, int z, int i, int j) : D1(y,z), D2(i,j) { e=x;}
+
+//Теперь добавим в каждый класс функцию show, которая бы выводила на экран переменную из секции private класса, которому принадлежит сама и вызывала бы функции show тех классов, которые стоят выше по иерархии наследования.
+ void show_B() { cout <<"B=   "<< a << "\n"; }
+
+ void show_D1() { cout << "D1=  "<< b << "\n"; show_B();}
+
+ void show_D2() { cout <<"D2=  "<< c << "\n"; show_B();}
+
+ void show_D3() { cout << "D3=  "<<d << "\n"; show_D1(); show_D2();}
+
+ void show_D4() { cout <<"D4=  "<< e << "\n"; show_D1(); show_D2();}
+
+//Демонстрация иерархии наследования
+main() {
+clrscr();
+D3 temp(100,200,300,400,500);
+D4 temp1(1,2,3,4,5);
+cout << "D3 temp(100,200,300,400,500);\n";
+cout << "D4 temp1(1,2,3,4,5);\n";
+cout<< "\nСледуя иерархии класса D3: \n";
+temp.show_D3();
+cout<< "\nСледуя иерархии класса D4\n";
+temp1.show_D4();
+getch();
+return 0;}
--- Course III/УП/Практики/Практика 12/program1.cpp
+++ Course III/УП/Практики/Практика 12/program1.cpp
@@ -1,103 +1,214 @@
 /*
-Создать класс четырехугольник, члены класса – координаты 4-хточек.
-Предусмотреть в классе методы проверки существования четырехугольника вычисления и 
-вывода сведений о фигуре – длины сторон, диагоналей, периметр, площадь. 
-Создать производный класс –параллелограмм, предусмотреть в классе проверку, 
-является ли фигура параллелограммом. 
 
-Написать программу, демонстрирующую работу с классом: дано N 
-четырехугольников и M параллелограммов, найти среднюю площадь N 
-четырехугольников и параллелограммы наименьшей и наибольшей площади.
+    Создать класс четырехугольник, члены класса – координаты 4-хточек.
+    Предусмотреть в классе методы проверки существования четырехугольника
+    вычисления и вывода сведений о фигуре –
+    длины сторон, диагоналей, периметр, площадь.
+    Создать производный класс –параллелограмм, предусмотреть в классе проверку, 
+    является ли фигура параллелограммом.
+
+    Написать программу, демонстрирующую работу с классом:
+    дано N четырехугольников и M параллелограммов,
+    найти среднюю площадь N четырехугольников и параллелограммы
+    наименьшей и наибольшей площади.
+
 */
 
-#include <vector>
-#include <map>
-#include <string>
-#include <cstdlib>
-#include <iostream>
-#include <math.h>
-
+#include<iostream>
+#include<cmath>
 using namespace std;
-class figure {
-protected:
-	int point1[2];
-	int point2[2];
-	int point3[2];
-	int point4[2];
+bool sys = false;
+class QuadrangleClass
+{
+    
+public:
+    double s[3];
+    double AB, BC, CD, AD;
+};
+
+
+class DefaultClass: public QuadrangleClass
+{
 
 public:
-    /*figure()
-    {
-        a = 0;
-        b = 0;
-        c = 0;
-        d = 0;
-    }
-*/
-    figure(int point1_buf,int point2_buf,int point3_buf,int point4_buf)
-    {
-        
-        
-    }
 
-/*
-    bool proverka()
-    {
-        return abs(a - b) == abs(c - d) && abs(a - c) == abs(b - d);
+    DefaultClass(double s1, double s2, double s3, double s4) {
+        s[0] = s1;
+        s[1] = s2;
+        s[2] = s3;
+        s[3] = s4;
     }
-
-    int perimentr()
-    { 
-    	return 2 * (a + b);
+    
+    int sychestv() {
+        if ((s[0]<(s[1]+s[2]+s[3])) && 
+            (s[1]<(s[0]+s[2]+s[3])) &&
+            (s[2]<(s[0]+s[1]+s[3])) &&
+            (s[3]<(s[0]+s[1]+s[2]))
+            )
+            return 1;
+        return 0;
     }
+    double perimetr() {
+            double p = s[0] + s[1] + s[2] + s[3];
+            cout << "P = "<<p<<"\n";
+            return p;
+    }
+    double ploschad() {
 
-    int area() { return abs(a - b) * abs(a - c); }
+        double p = perimetr();
+        double s = sqrt((p-s[0])*(p-s[1])*(p-s[2])*(p-s[3]));
+        cout << "S = "<<s<<"\n";
+        return s;
+    }
+    /*double ygl() {
 
-    double diagonal() {
-    	return sqrt(((a - b) * (a - b)) + ((a - c) * (a - c))); }
-	};
-*/
+
+        double a = (acos(((s[1]* s[1])+ (s[2]* s[2])- (s[0]* s[0]))/(2* s[1]* s[2]))*180/3.14159);
+        double b = (acos((pow(s[0], 2) + pow(s[2], 2) - pow(s[1], 2)) / (2 * s[0] * s[2])) * 180 / 3.14159);
+        double c = 180 - (a + b);
+        cout << "Углы треугольника " << a << " " << b << " " << c <<endl;
+        double ufd[3];
+        ufd[0]=a;
+        ufd[1] = b;
+        ufd[2] = c;
+        return ufd[3];
+    }
+    */
+
+    void storona(){
+        cout<<"   B +-----------+ C\n";
+        cout<<"    /            | \n";
+        cout<<"   /             | \n";
+        cout<<"A +--------------+ D\n";
+        cout<<"A = "<<s[0]<<"\nB = "<<s[1]<<"\nC = "<<s[2]<<"\nD = "<<s[3]<<"\n";
+        cout<<"AB = "<<s[1]-s[0]<<"\n";
+        cout<<"BC = "<<s[2]-s[1]<<"\n";
+        cout<<"CD = "<<s[3]-s[2]<<"\n";
+        cout<<"AD = "<<
+
+    }
 };
-/*
-class parale : public figure {
-    double f;
 
+class ParallelClass: public QuadrangleClass
+{
 public:
-    parale(void)
-        : figure(0, 0, 0, 0)
+    ParallelClass(double s11, double s21, double s31, double s41) 
     {
+        s[0] = s11;
+        s[1] = s21;
+        s[2] = s31;
+        s[3] = s41;
     }
 
-    parale(int a1, int b1, int c1, int d1)
-        : figure(a1, b1, c1, d1)
-    {
+    int  sychestv() {
+        double d1, d2, d3, d4;
+        d1 = s[0] * s[0];
+        d2 = s[1] * s[1];
+        d3 = s[2] * s[2];
+        if ((d1 == d2 + d3) || (d2 == d1 + d3) || (d3 == d1 + d2))
+        {
+            return 1;
+        }
+            
+        else 
+        {
+            return 0;
+        }
     }
-
-    void setF(double f1) { f = f1; }
-    double getF() {
-    	return f;
-    }
-    double getG() { return 180 - f; }
 };
-*/
-int main(int argc, char* argv[]){
 
-	int point1[2] = {1,1};
-	int point2[2] = {1,1};
-	int point3[2] = {1,1};
-	int point4[2] = {1,1};
+int main() {
+    setlocale(LC_ALL, "rus");
+    int n, m,o;
+    cout << "n= " ;
+    cin >> n;
+    cout << "m= ";
+    cin >> m;
+    bool k = false, k1 = false;
+    double *a, *b, *c,*pl,*a1,*b1,*c1,*gyp;
+    a = new double[n];
+    b = new double[n];
+    c = new double[n];
+    pl = new double[n];
+    a1 = new double[m];
+    b1 = new double[m];
+    c1 = new double[m];
+    gyp = new double[m];
 
-	map <bool, string> formatter;
-		formatter[true] = "Проверку прошел!";
-		formatter[false] = "Проверку не прошел(";
+    cout << "Введите длины сторон треугольника для простых треугольников через пробел " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "i[" << i << "]= ";
+        cin >> a[i] 
+            >> b[i] 
+            >> c[i];
+    }
+        
+    for (int i = 0; i < n; i++) {
+        DefaultClass ob1(a[i], b[i], c[i]);
+        if (ob1.sychestv() == 1)
+        {
+            cout << "Треугольник со сторонами " << a[i]<<" " << b[i]<<" " << c[i] << " существует"<<endl;
+            ob1.perimetr();
+            ob1.ploschad();
+            ob1.ygl();
+            pl[i] = ob1.ploschad();
+            k1 = true;
+        }
+        else {
+            cout << "Треугольник со сторонами " << a[i] << " " << b[i] << " " << c[i] << " не существует" << endl;
+            pl[i] = 0;
+        }
+    }
 
+    cout << "Введите длины сторон треугольника для прямоугольных треугольников через пробел " << endl;
+    for (int i = 0; i < m; i++) {
+        cout << "i[" << i << "]= ";
+        cin >> a1[i]
+            >> b1[i]
+            >> c1[i];
+    }
 
-	//vector myVector1[0, 1];
-    figure obj1(point1,point2,point3,point4);
-
-    //cout<<formatter[obj1.proverka()];
-    //cout<<"Периметр:"<<obj1.perimentr();
-
-
-cout<<"\n";
+    for (int i = 0; i < m; i++) {
+        ParallelClass ob1(a1[i], b1[i], c1[i]);
+        if (ob1.sychestv() == 1)
+        {
+            cout << "Треугольник со сторонами " << a1[i] << " " << b1[i] << " " << c1[i] << " прямоугольный" << endl;
+            k = true;
+            gyp[i] = a1[1];
+        }
+        else {
+            cout << "Треугольник со сторонами " << a1[i] << " " << b1[i] << " " << c1[i] << " не прямоугольный" << endl;
+            gyp[i] = -1000;
+        }
+    }
+    double maxpl = pl[0];
+    int ind=0;
+    for (int i = 0; i < n; i++) 
+    {
+        if (pl[i] > maxpl) {
+            maxpl = pl[i];
+            ind = i;
+        }
+            
+    }
+    double mingyp = gyp[0];
+    int ind1 = 0;
+    for (int i = 0; i < m; i++)
+    {
+        if (gyp[i] < mingyp && gyp[i] != -1000) {
+            ind1 = i;
+            mingyp = gyp[i];
+        }
+    }
+    if (k1 == true) {
+        cout << "Максимальная площадь равна" << maxpl << " при длине сторон " << a[ind] << " " << b[ind] << " " << c[ind] << endl;
+    }
+    else
+        cout << "Нет ни одного треугодьника" << endl;
+    if (k == true) {
+        cout << "Минимальная гипотенуза" << mingyp << " при длине сторон " << a1[ind1] << " " << b1[ind1] << " " << c1[ind1] << endl;
+    }
+    else
+        cout << "Нет ни одного треугодьника" << endl;
+    system("pause");
 }
