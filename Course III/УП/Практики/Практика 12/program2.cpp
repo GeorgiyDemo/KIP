f34c2ac002d6ee35d648a530e22c2d5d0f6adf27
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-12-05 18:36:05 +0300

    УП Pract 12 №2 changed

--- Course III/УП/Практики/Практика 12/program2.cpp
+++ Course III/УП/Практики/Практика 12/program2.cpp
@@ -1,51 +1,65 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
+class B2 {
+    int e;
 
-class B1{
- int a;
 public:
- B1() { };
- B1(int x) { a=x; }
- void show_B1() { cout <<"B1=   "<< a << "\n"; }
+    B2(int x, int y){};
+    B2(int x) { e = x; }
+    void show_B2() { cout << "B2=  " << e << "\n"; }
 };
 
-class B2{
- int a;
+class B1 {
+    int a;
+
 public:
- B2() { };
- B2(int x) { a=x; }
- void show_B2() { cout <<"B2=   "<< a << "\n"; }
+    B1(int x, int y, int i, int j){};
+    B1(int x) { a = x; }
+    void show_B1() { cout << "B1=  " << a << "\n"; }
 };
 
-class D1: private B2, public B1 {
- int d;
+class D1 : private B2, public B1 {
+    int b;
+
 public:
- D1(int x, int y, int z, int i, int j) : B2(y,z), B1(i,j) { d=x;}
- void show_D1() { cout << "D1=  "<<d << "\n"; show_B2(); show_B1();}
+    D1(int x, int y,int i,int u) : B2(x,y), B1(i) { b = x; };
+
+    void show_D1()
+    {
+        cout << "D1=  " << b << "\n";
+        show_B1();
+        show_B2();
+    }
 };
 
-class D2: private D1{
- int c;
+
+
+class D2 : public D1 {
+	int c;
 public:
- D2(int x, int y) : D1(y) { c=x;};
- void show_D2() { cout <<"D2=  "<< c << "\n"; show_B();}
+	D2(int x, int y,int i,int u) :  D1(x, y, i,u) { c = i; }
+	void show_D2() { cout << "D2=  " << c << "\n";  show_D1(); }
 };
 
-class D3: public D2{
- int c;
+
+class D3 : public D2 {
+	int d;
 public:
- D3(int x, int y) : B(y) { c=x;};
- void show_D3() { cout <<"D3=  "<< c << "\n"; show_B();}
+	D3(int x, int y,int i,int u) :  D2(x, y, i,u) { d = i; }
+	void show_D3() { cout << "D3=  " << d << "\n";  show_D2(); }
 };
 
-int main() {
-D3 temp(100,200,300,400,500);
-D4 temp1(1,2,3,4,5);
-cout << "D1 temp(100,200,300,400,500);\n";
-cout << "D4 temp1(1,2,3,4,5);\n";
-cout<< "\nСледуя иерархии класса D1: \n";
-temp.show_D2();
-cout<< "\nСледуя иерархии класса D4\n";
-temp1.show_D3();
-return 0;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    D3 temp(100, 200, 300, 400);
+
+    /*
+	cout << "D4 temp1(1,2,3,4);\n";*/
+    cout << "\nПолученная иерархия: \n";
+    temp.show_D3();
+
+    return 0;
 }
