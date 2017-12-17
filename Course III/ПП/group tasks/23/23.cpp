7a1ec2a3af229a60641c7cf3e0e35c10cb5dce7e
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-12-17 15:29:24 +0300

    ПП Group tasks 14-17 docx added

--- Course III/ПП/group tasks/23/23.cpp
+++ Course III/ПП/group tasks/23/23.cpp
@@ -1,59 +1,15 @@
-//Задачи 16,23
+/*
+Описать класс с именем WORKER, содержащую следующие поля: 
+• фамилия работника;
+• название занимаемой должности; 
+• год поступления на работу. 
+Написать программу, выполняющую следующие действия: ввод с клавиатуры из файла данных в массив, состоящий из двух элементов типа WORKER; вывод на дисплей и в файл данных обо всех работниках. Файловый ввод вывод выполнить в стиле С и С++ Обработать исключение, если файл не открывается
+*/
+
 #include <fstream>
 #include <iostream>
 using namespace std;
-int maxc(int a, int b)
-{
-    if (a > b)
-        return a;
-    else
-        return b;
-}
-double maxc(double a, double b)
-{
-    if (a > b)
-        return a;
-    else
-        return b;
-}
-char maxc(char a, char b)
-{
-    if (a > b)
-        return a;
-    else
-        return b;
-}
-class Max {
-    char cmax;
-    int imax;
-    double dmax;
 
-public:
-    Max(int a, int b)
-    {
-        if (a > b)
-            imax = a;
-        else
-            imax = b;
-    }
-    Max(char a, char b)
-    {
-        if (a > b)
-            cmax = a;
-        else
-            cmax = b;
-    }
-    Max(double a, double b)
-    {
-        if (a > b)
-            dmax = a;
-        else
-            dmax = b;
-    }
-    int GetIntMax() { return imax; }
-    double GetDoubleMax() { return dmax; }
-    char GetCharMax() { return cmax; }
-};
 class Worker {
     string name;
     string jobname;
@@ -100,38 +56,6 @@ TagInfo GetTagInner(string opentag, string closetag, string code, int pos = 0)
 }
 int main()
 {
-    /*
-#16
-Создайте перегруженную функцию maxc() ,
-которая находит максимум для двух переменных типа целые (int), вещественные
-(double), символьные (char).
-a. С помощью перегруженных функций (без классов) b. С помощью перегруженных
-конструкторов или методов класса
-*/
-    cout << "#16"
-         << "\n";
-    cout << "a."
-         << "\n";
-    cout << "maxc(1,2)=" << maxc(1, 2) << "\n";
-    cout << "maxc(1.0,2.0)=" << maxc(1.0, 2.0) << "\n";
-    cout << "maxc('a','b')=" << maxc('a', 'b') << "\n";
-    cout << "b."
-         << "\n";
-    cout << "Max(1,2).GetIntMax()=" << Max(1, 2).GetIntMax() << "\n";
-    cout << "Max(1.0,2.0).GetDoubleMax()=" << Max(1.0, 2.0).GetDoubleMax() << "\n";
-    cout << "Max('a','b').GetCharMax()=" << Max('a', 'b').GetCharMax() << "\n";
-    /*
-#23
-Описать класс с именем WORKER, содержащую следующие поля:
-• фамилия работника;
-• название занимаемой должности;
-• год поступления на работу.
-Написать программу, выполняющую следующие действия: ввод с клавиатуры из
-файла данных в массив,
-состоящий из двух элементов типа WORKER; вывод на дисплей и в файл данных
-обо всех работниках.
-Файловый ввод вывод выполнить в стиле С или С++
-*/
     Worker* arr[2];
     for (int i = 0; i <= 2; i++) {
         string name, jobname, year;
