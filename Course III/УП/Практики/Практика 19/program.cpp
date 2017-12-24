57b9c6c22e58debee5dee6b021ca290b173d080d
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-12-25 01:17:26 +0300

    УП Pract 19 ready

--- Course III/УП/Практики/Практика 19/program.cpp
+++ Course III/УП/Практики/Практика 19/program.cpp
@@ -1,84 +1,74 @@
 /*
-
 4.	Написать программу с обобщенным  классом,
 	у которого есть поле -  массив.
 	Перегрузить оператор инкремента так, чтобы все
 	столбцы в массиве циклически сдвигались на одну позицию.
 
-*/
- 
+ */
+
 #include <iostream>
 #include <string>
-#include <cstdlib>
- 
 using namespace std;
- 
-template<class X, int n> class Abc
+
+template<class KOT, int n> class KomukClass
 {
+protected:
+    KOT main_arr[n];
 public:
-    X x[n];
- 
-    X operator[](int k)
+    KOT operator[](int k)
     {
-        return x[k % n];
+        return main_arr[k % n];
     }
- 
-    Abc operator++(int m)
+
+    KomukClass operator++(int m)
     {
-        X tmp;
-        tmp = x[n - 1];
- 
+        KOT tmp_obj;
+        tmp_obj = main_arr[n - 1];
         for (int i = n - 1; i > 0; i--)
-            x[i] = x[i - 1];
- 
-        x[0] = tmp;
+            main_arr[i] = main_arr[i - 1];
+        main_arr[0] = tmp_obj;
         return *this;
     }
- 
-    Abc()
-    {
-        srand(1);
-        for (int i = 0; i < n; i++)
-        {
-            x[i] = (X)(rand() % 100);
-        }
-    }
- 
-    Abc(string str)
+
+    KomukClass()
     {
         for (int i = 0; i < n; i++)
-        {
-            x[i] = str[i];
-        }
+            main_arr[i] = (KOT)(rand() % 99);
     }
- 
-    void ShowValue()
+
+    KomukClass(string str)
+    {
+        for (int i = 0; i < n; i++)
+            main_arr[i] = str[i];
+    }
+
+    void OutArray()
     {
         cout<<"[";
         for (int i = 0; i < n; i++)
-        {
-            cout << x[i] << " ";
-        }
+            cout << main_arr[i] << " ";
         cout<<"]";
     }
 };
- 
+
 int main()
 {
-    Abc<int, 10> abc;
-    cout << "Массив чисел до сдвига:\n";
-    abc.ShowValue();
-    abc++;
+    srand(time(NULL));
+
+    KomukClass<int, 10> obj;
+    cout << "\nМассив чисел до сдвига:\n";
+    obj.OutArray();
+    obj++;
     cout << "\nМассив чисел после сдвига:\n";
-    abc.ShowValue();
- 
-    Abc<string, 13> abc2("KOT_MEOW_MEOW");
+    obj.OutArray();
+
+    KomukClass<string, 4> obj1("KOT_MEOW_MEOW");
     cout << "\n\nМассив строк до сдвига:\n";
-    abc2.ShowValue();
-    abc2++;
+    obj1.OutArray();
+    obj1++;
     cout << "\nМассив строк после сдвига:\n";
-    abc2.ShowValue();
- 
+    obj1.OutArray();
+
     cout<<"\n";
     return 0;
 }
