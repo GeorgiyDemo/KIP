d0d3f82d355b42fc4dd98eac9e79620d80ed29c8
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-12-27 14:30:14 +0300

    ЧМ C++ Gauss

--- Course III/ЧМ/C++/Gauss.cpp
+++ Course III/ЧМ/C++/Gauss.cpp
@@ -1,99 +1,110 @@
 #include "stdafx.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <locale.h>
-
-void change(double** nc, int i1, int i2, int n)
+#include <iostream>
+using namespace std;
+// Вывод системы уравнений
+void sysout(double **a, double *y, int n)
 {
-    int j;
-    double* buf;
-
-    buf = (double*)malloc((n + 1) * sizeof(double));
-    for (j = 0; j < n + 1; j++) {
-        buf[j] = nc[i1][j];
-        nc[i1][j] = nc[i2][j];
-        nc[i2][j] = buf[j];
-    }
-}
-
-void main()
-{
-    double **data, k, help, *helper;
-    int i, j, l, counter, hlp, n;
-    setlocale(LC_ALL, "rus");
-    printf("Введите порядок системы: ");
-    scanf_s("%d", &n);
-    data = (double**)malloc(n * sizeof(double*));
-    for (i = 0; i < n; i++)
-        data[i] = (double*)malloc((n + 1) * sizeof(double));
-    printf("Вводите элементы расширенной матрицы: \n");
-    for (i = 0; i < n; i++)
-        for (j = 0; j < n + 1; j++) {
-            printf("Введите элемент %d-%d: ", i + 1, j + 1);
-            scanf_s("%lf", &data[i][j]);
-        }
-    printf("Ваша расширенная матрица: ");
-    for (i = 0; i < n; i++) {
-        printf("\n");
-        for (j = 0; j < n + 1; j++)
-            printf("%5.3lf ", data[i][j]);
-    }
-    for (i = 0; i < n - 1; i++) {
-        hlp = 0;
-        do {
-            help = data[i][i];
-            if (help == 0) {
-                counter = 0;
-                for (l = i + 1; l < n + 1; l++)
-                    if (data[i][l] == 0)
-                        counter++;
-                //if (counter == n-i)
-                change(data, i, n - hlp, n);
-            }
-            hlp++;
-        } while ((help == 0) || (hlp > n - i));
-        if (hlp > n - i) {
-            printf("\nСистема несовместна, решений нет");
-            exit(0);
-        }
-        for (j = i + 1; j < n; j++) {
-            k = -data[j][i] / help;
-            for (l = i; l < n + 1; l++)
-                data[j][l] = data[i][l] * k + data[j][l];
-        }
-    }
-    printf("\nРезультат прямого шага Гаусса:\n");
-    for (l = 0; l < n; l++) {
-        printf("\n");
-        for (j = 0; j < n + 1; j++)
-            printf("%5.3lf ", data[l][j]);
-    }
-    helper = (double*)malloc(n * sizeof(double));
-    //Обратный шаг
-
-    if ((data[n - 1][n] * data[n - 1][n - 1] == 0) && ((data[n - 1][n] != 0 || data[n - 1][n - 1] != 0))) {
-        printf("\nСистема несовместна, решений нет");
-        exit(0);
-    }
-    else {
-        for (i = n - 1; i >= 0; i--) {
-            for (j = 0; j < n; j++)
-                if (j != i)
-                    data[i][n] -= data[i][j];
-            if ((data[i][n] == 0) && (data[i][i] == 0)) {
-                printf("Cложный алгоритм, напишу потом может быть, а то уже полвторого");
-            }
-            helper[i] = data[i][n] / data[i][i];
-            for (j = 0; j < i; j++)
-                data[j][i] *= helper[i];
-        }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
         {
-            printf("\nРешение уравнения: (");
-            for (i = 0; i < n; i++)
-                printf("%5.3lf ", helper[i]);
-            printf(")");
+            cout << a[i][j] << "*x" << j;
+            if (j < n - 1)
+                cout << " + ";
+        }
+        cout << " = " << y[i] << endl;
+    }
+    return;
+}
+double * gauss(double **a, double *y, int n)
+{
+    double *x, max;
+    int k, index;
+    const double eps = 0.00001;  // точность
+    x = new double[n];
+    k = 0;
+    while (k < n)
+    {
+        // Поиск строки с максимальным a[i][k]
+        max = abs(a[k][k]);
+        index = k;
+        for (int i = k + 1; i < n; i++)
+        {
+            if (abs(a[i][k]) > max)
+            {
+                max = abs(a[i][k]);
+                index = i;
+            }
+        }
+        // Перестановка строк
+        if (max < eps)
+        {
+            // нет ненулевых диагональных элементов
+            cout << "Решение получить невозможно из-за нулевого столбца ";
+            cout << index << " матрицы A" << endl;
+            return 0;
+        }
+        for (int j = 0; j < n; j++)
+        {
+            double temp = a[k][j];
+            a[k][j] = a[index][j];
+            a[index][j] = temp;
+        }
+        double temp = y[k];
+        y[k] = y[index];
+        y[index] = temp;
+        // Нормализация уравнений
+        for (int i = k; i < n; i++)
+        {
+            double temp = a[i][k];
+            if (abs(temp) < eps) continue; // для нулевого коэффициента пропустить
+            for (int j = 0; j < n; j++)
+                a[i][j] = a[i][j] / temp;
+            y[i] = y[i] / temp;
+            if (i == k)  continue; // уравнение не вычитать само из себя
+            for (int j = 0; j < n; j++)
+                a[i][j] = a[i][j] - a[k][j];
+            y[i] = y[i] - y[k];
+        }
+        k++;
+    }
+    // обратная подстановка
+    for (k = n - 1; k >= 0; k--)
+    {
+        x[k] = y[k];
+        for (int i = 0; i < k; i++)
+            y[i] = y[i] - a[i][k] * x[k];
+    }
+    return x;
+}
+int main()
+{
+    double **a, *y, *x;
+    int n;
+    system("chcp 1251");
+    system("cls");
+    cout << "Введите количество уравнений: ";
+    cin >> n;
+    a = new double*[n];
+    y = new double[n];
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = new double[n];
+        for (int j = 0; j < n; j++)
+        {
+            cout << "a[" << i << "][" << j << "]= ";
+            cin >> a[i][j];
         }
     }
-    system("pause");
+    for (int i = 0; i < n; i++)
+    {
+        cout << "y[" << i << "]= ";
+        cin >> y[i];
+    }
+    sysout(a, y, n);
+    x = gauss(a, y, n);
+    for (int i = 0; i < n; i++)
+        cout << "x[" << i << "]=" << x[i] << endl;
+    cin.get(); cin.get();
+    return 0;
 }
