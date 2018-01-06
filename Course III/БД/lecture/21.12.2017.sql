46ee8bf0c450bb698ec1d01d5d4c73ddded5fecb
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-01-07 01:20:30 +0300

    БД lectures updated

---
+++ Course III/БД/lecture/21.12.2017.sql
@@ -0,0 +1,15 @@
+
+--Допустим, нужно получить список продавцов с их ткущими заказами. Нуж
+SELECT salespeople.sname, orders.onum, orders.odate FROM Salespeople JOIN Orders 
+ON Salespeople.snum = orders.snum;
+
+--Нужно получить все заказы с соответствующими именами продавцов  и покупателей.
+SELECT sname, cname, onum
+FROM Salespeople, Customers, Orders
+WHERE Orders.snum = Salespeople.snum;
+and Orders.cnum = Customers.cnum;
+
+--Найти все пары покупателей, имеющих одинаковый рейтинг
+SELECT first.cname, second.cname, first.rating
+FROM Customers.first, Customers.second
+WHERE first.rating = second.rating;
