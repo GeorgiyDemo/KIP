b98fd04715166b90b4983da5eef2faae4b54dfb0
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-02-21 11:14:54 +0300

    ПО C# pract 1/2 added

---
+++ Course III/ПО/Task1/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите строку => ");
+            string meow = Console.ReadLine();
+            Console.Write("Введите число => ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+                Console.WriteLine(Convert.ToString(i + 1) + " " + meow);
+        }
+    }
+}
---
+++ Course III/ПО/Task2/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 0;
+            Console.Write("Введите число №1 => ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите число №2 => ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("1. Умножение\n2. Деление\n3. Вычитание\n4. Сложение");
+                n = Convert.ToInt32(Console.ReadLine());
+               
+                Console.WriteLine("Результат: ");
+                switch (n)
+                {
+                    case 1:
+                        Console.WriteLine(a * b);
+                        break;
+                    case 2:
+                        Console.WriteLine(a / b);
+                        break;
+                    case 3:
+                        Console.WriteLine(a - b);
+                        break;
+                    case 4:
+                        Console.WriteLine(a + b);
+                        break;
+
+                }
+
+            }
+            while (n != 0);
+               
+        }
+    }
+}
