14d4db37fa9cb9e41624ba5340ee3624d8848fa6
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-02-27 21:28:51 +0300

    ПО C# pract 4 added (not full)

---
+++ Course III/ПО/Tesk4/Tesk4/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tesk4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcentButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void KorenButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExtentButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void FractionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlusMinusButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NullButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PointButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ResultButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlusButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EmptyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EmptyAllButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
