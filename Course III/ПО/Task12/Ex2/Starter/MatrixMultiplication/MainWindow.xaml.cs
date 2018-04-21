42c88789b4a35102ac42bd3c75fa5ac394205eca
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-04-21 14:49:30 +0300

    ПО Task 12 ex 2 added

--- Course III/ПО/Task12/Ex2/Starter/MatrixMultiplication/MainWindow.xaml.cs
+++ Course III/ПО/Task12/Ex2/Starter/MatrixMultiplication/MainWindow.xaml.cs
@@ -52,55 +52,33 @@ namespace MatrixMultiplication
             int m1rows = 1;
             int m1columns_m2rows = 1;
             int m2columns = 1;
-
-            // Set the matrix dimensions - note that matrix2 must have the same number of rows as matrix1 has columns
-            if (matrix1width != null) m1columns_m2rows = matrix1width.SelectedIndex +1;
+            if (matrix1width != null) m1columns_m2rows = matrix1width.SelectedIndex + 1;
             if (matrix1height != null) m1rows = matrix1height.SelectedIndex + 1;
             if (matrix2width != null) m2columns = matrix2width.SelectedIndex + 1;
-
-            // matrices will be initialized with all zeros
             matrix1 = new double[m1columns_m2rows, m1rows];
             matrix2 = new double[m2columns, m1columns_m2rows];
-            result = new double[m2columns,m1rows];
-
-            // Display the matrices on the form
+            result = new double[m2columns, m1rows];
             InitializeGrid(grid1, matrix1);
             InitializeGrid(grid2, matrix2);
             InitializeGrid(grid3, result);
         }
-
-        /// <summary>
-        /// Create a grid on the form displaying the matrix values in editable textboxes
-        /// </summary>
-        /// <param name="grid">A WPF grid control</param>
-        /// <param name="matrix">The matrix to display</param>
         private void InitializeGrid(Grid grid, double[,] matrix)
         {
             if (grid != null)
             {
-                // Reset the grid before doing anything
                 grid.Children.Clear();
                 grid.ColumnDefinitions.Clear();
                 grid.RowDefinitions.Clear();
-
-                // Get the dimensions
                 int columns = matrix.GetLength(0);
                 int rows = matrix.GetLength(1);
-
-                // Add the correct number of coumns to the grid
                 for (int x = 0; x < columns; x++)
                 {
-                    // GridUnitType.Star - The value is expressed as a weighted proportion of available space
                     grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star), });
                 }
-
                 for (int y = 0; y < rows; y++)
                 {
-                    // GridUnitType.Star - The value is expressed as a weighted proportion of available space
                     grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star), });
                 }
-
-                // Fill each cell of the grid with an editable TextBox containing the value from the matrix - centered in the cell
                 for (int x = 0; x < columns; x++)
                 {
                     for (int y = 0; y < rows; y++)
@@ -117,42 +95,28 @@ namespace MatrixMultiplication
                 }
             }
         }
-
-        /// <summary>
-        /// Do the matrix multiplication
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void ButtonCalculate_Click(object sender, RoutedEventArgs e)
         {
-            // Get the current values from the grid and save them in the arrays
             GetValuesFromGrid(grid1, matrix1);
             GetValuesFromGrid(grid2, matrix2);
+            try
+            {
 
-            // TODO Implement exception handling for the MatrixMultiply method invocation
-            // Do the multiplication - checking for exceptions
-            result = Matrix.MatrixMultiply(matrix1, matrix2);
-
-            // Show the results
+                result = Matrix.MatrixMultiply(matrix1, matrix2);
+                throw new ArgumentException();
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
             InitializeGrid(grid3, result);
         }
-
-        /// <summary>
-        /// Retrieve data from the grid and put it in the array representing the matrix
-        /// </summary>
-        /// <param name="grid">WPF Grid control holding matrix data</param>
-        /// <param name="matrix">Matrix array to store the values</param>
         private void GetValuesFromGrid(Grid grid, double[,] matrix)
         {
-            // Set up counters
             int columns = grid.ColumnDefinitions.Count;
             int rows = grid.RowDefinitions.Count;
-
-            // Check grid has same dimensions as array
             if (columns != matrix.GetLength(0)) throw new ArgumentException("Grid and matrix have different number of columns");
             if (rows != matrix.GetLength(1)) throw new ArgumentException("Grid and matrix have different number of rows");
-
-            // Iterate over cells in Grid, copying to matrix array
             for (int c = 0; c < grid.Children.Count; c++)
             {
                 TextBox t = (TextBox)grid.Children[c];
--- Course III/ПО/Task12/Ex2/Starter/MatrixMultiplication/Matrix.cs
+++ Course III/ПО/Task12/Ex2/Starter/MatrixMultiplication/Matrix.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 
 namespace MatrixMultiplication
 {
@@ -21,34 +22,57 @@ namespace MatrixMultiplication
         public static double[,] MatrixMultiply(double[,] matrix1, double[,] matrix2)
         {
             // TODO Evaluate input matrices for compatibility
-            // Check the matrices are compatible            
-            
-            // Get the dimensions
+            // Check the matrices are compatible 
+            try
+            {
+                if (matrix1.Length != matrix2.Length)
+                    throw new ArgumentException();
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Число строк и столбцов должно совпадать");
+            }
+
+            try
+            {
+                if (matrix1.Length != matrix2.Length)
+                    throw new ArgumentException();
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Число строк и столбцов должно совпадать");
+            }
+
             int m1columns_m2rows = matrix1.GetLength(0);
             int m1rows = matrix1.GetLength(1);
             int m2columns = matrix2.GetLength(0);
-
-            // Create a suitable array to hold the result of the multiplication
             double[,] result = new double[m2columns, m1rows];
-
-            // Calculate the value for each cell in the result matrix
-            for (int row = 0; row < m1rows; row++)
+            try
             {
-                for (int column = 0; column < m2columns; column++)
+                for (int row = 0; row < m1rows; row++)
                 {
-                    // Initialize the value for the result cell
-                    double accumulator = 0;
-                    
-                    // Iterate over the row in the source matrix multiplying by the value in the column in the second matrix, add to the accumulator
-                    for (int cell = 0; cell < m1columns_m2rows; cell++)
+                    for (int column = 0; column < m2columns; column++)
                     {
-                        // TODO Evaluate matrix data points for invalid data
-                        // Throw exceptions if either matrix contains a negative entry
-                        accumulator += matrix1[cell, row] * matrix2[column, cell];
+                        double accumulator = 0;
+                        for (int cell = 0; cell < m1columns_m2rows; cell++)
+                        {
+                            if (matrix1[column, cell] < 0)
+                            {
+                                throw new ArgumentException(string.Format(" Matrix1 содержит неправильное значение в ячейке[{0}, {1}].", column + 1, cell + 1));
+                            }
+                            if (matrix2[column, cell] < 0)
+                            {
+                                throw new ArgumentException(string.Format(" Matrix2 содержит неправильное значение в ячейке[{0}, {1}].", column + 1, cell + 1));
+                            }
+                            accumulator += matrix1[cell, row] * matrix2[column, cell];
+                        }
+                        result[column, row] = accumulator;
                     }
-                    result[column, row] = accumulator;
                 }
+
             }
+            catch (ArgumentException ex)
+            { MessageBox.Show(ex.Message); }
             return result;
         }
     }
