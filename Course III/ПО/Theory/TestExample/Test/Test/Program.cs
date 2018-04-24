bd453b68629fee37a7dcde4d147745ab6be5a00f
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-04-24 14:51:12 +0300

    ПО Unit tests example added

---
+++ Course III/ПО/Theory/TestExample/Test/Test/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Test
+{
+    public class Program
+    {
+        public Program() { }
+
+        public int adder(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int subdevided(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int multipy(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double delete(int a, int b)
+        {
+            return (double)a / (double)b;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
