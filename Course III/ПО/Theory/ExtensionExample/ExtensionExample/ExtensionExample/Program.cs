c878b3c3a5260b87ee8c7544237026d7af82034d
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-05-04 15:09:25 +0300

    ПО Theory ExtensionExample added

---
+++ Course III/ПО/Theory/ExtensionExample/ExtensionExample/ExtensionExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExtensionExample
+{
+
+    static class MyClass{
+        public static double Stepen(this double n, int p){
+            double result=1.0;
+            for (int i = 0; i < p;i++){
+                result *= n;
+            }
+            return result;
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите исходное число => ");
+            double n = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите число для возведения в степень => ");
+            int p = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(n.Stepen(p));
+        }
+    }
+}
