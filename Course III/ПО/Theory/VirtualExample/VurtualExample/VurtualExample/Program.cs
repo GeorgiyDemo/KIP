57e4da4d215136b564cb79084d3479ac7873ca41
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-05-11 14:21:52 +0300

    ПО Theory VirtualExample added

---
+++ Course III/ПО/Theory/VirtualExample/VurtualExample/VurtualExample/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace VurtualExample
+{
+
+    public class A
+    {
+        public int a;
+        public A(int a){
+            this.a = a; 
+        }
+
+        public virtual void ShowString(){
+            Console.WriteLine("Метод класса А " + a); 
+        } 
+         
+    }
+
+    class B :  A
+    {
+        public int b;
+
+        public B(int b, int a): base(a){
+            this.b = b;
+       }
+
+        public override void ShowString(){
+            Console.WriteLine("Метод класса B "+ b); 
+        }
+
+    }
+
+     class C : A
+    {
+        public int c;
+
+        public C(int c, int a) : base(a){
+            this.c = c;
+
+        }
+        public new void ShowString(){
+            Console.WriteLine("Метод класса C " + c); 
+        } 
+
+    }  
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            A a_obj = new A(2);
+            a_obj.ShowString();
+
+            B b_obj = new B(3,2);
+            b_obj.ShowString();
+
+            C c_obj = new C(4,2);
+            c_obj.ShowString();
+        }
+    
+    }
+}
