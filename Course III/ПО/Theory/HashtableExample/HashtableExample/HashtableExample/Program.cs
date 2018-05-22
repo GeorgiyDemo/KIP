1ee5c14239a8a362e20ae2c01898b6ddafce85d1
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-05-22 15:24:48 +0300

    ПО Theory HashtableExample added

---
+++ Course III/ПО/Theory/HashtableExample/HashtableExample/HashtableExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApplication1
+{
+    class mem
+    {
+       
+        public void TableEnter()
+        {
+
+            Hashtable ht = new Hashtable()
+            {
+                {"GENA", "12345"},
+                {"MEOW", "kot123"},
+                {"WOW", "DOGE"},
+                {"NASTYA", "4321"}
+            };
+        
+            ICollection keys = ht.Keys;
+
+            foreach (string s in keys)
+                Console.WriteLine(s + ": " + ht[s]);
+
+            Console.WriteLine("-----------------------\nПосле удаления:\n---------------------");
+            ht.Remove("GENA");
+            ht.Remove("NASTYA");
+
+            foreach (string s in keys)
+                Console.WriteLine(s + ": " + ht[s]);
+            Console.ReadLine();    
+        }  
+    }
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+            mem mem_obj = new mem();
+            mem_obj.TableEnter();
+        }
+    }
+}
