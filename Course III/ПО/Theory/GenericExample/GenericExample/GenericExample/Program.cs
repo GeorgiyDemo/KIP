eebbadbbf01f385c843896679ece64241eefd437
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-05-24 19:01:52 +0300

    ПО Theory GenericExample added

---
+++ Course III/ПО/Theory/GenericExample/GenericExample/GenericExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericExample
+{
+    class Program
+    {
+        static T max<T> (T a,T b) where T: IComparable
+        {
+            T max = a;
+            if (b.CompareTo(max)>0)
+                max = b;
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(max(2,2.3));
+        }
+    }
+}
