63f098b58a653fb9825492f36ec5e0b74af88fa3
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-05-25 14:22:40 +0300

    ПО Theory Examples DIR changed

---
+++ Course III/ПО/Theory/GenericClassExample (3)/GenericClassExample/GenericClassExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace GenericClassExample
+{
+
+    public class meow<T> where T: IComparable<T>
+    {
+        public void add(T item1, T item2)
+        {
+            T result = (dynamic)item1 + item2;
+            Console.WriteLine(result);
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            meow<int> meow_obj = new meow<int>();
+            meow_obj.add(1, 2);
+
+            meow<string> meow_obj1 = new meow<string>();
+            meow_obj1.add("MEOW", "KOT");
+        }
+    }
+}
---
+++ Course III/ПО/Theory/GenericDelegateExample (1)/GenericDelegateExample/GenericDelegateExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GenericDelegateExample
+{
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int i;
+            Action<int> first = null;
+            Func<int, int> second = null;
+
+            first += (x) => Console.WriteLine("first = {0}", x++);
+            first += (x) => Console.WriteLine("meme = {0}", x -= 8);
+
+            second = (x) => {
+                Console.WriteLine("second = {0}, {0}", x -= 15);
+                return x;
+            };
+
+            second += (x) => {
+                Console.WriteLine("second = {0}, {0}", x -= 15);
+                return x;
+            };
+
+            first(6);
+            i = (int) second(7);
+            Console.WriteLine("i={0}", i);
+
+        }
+    }
+}
