d66907714aea06bfc9707f010c24d8c0e4400e34
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-05-25 14:18:36 +0300

    ПО Theory DelegateGenericExample added

---
+++ Course III/ПО/Theory/DelegateGenericExample/DelegateGenericExample/DelegateGenericExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DelegateGenericExample
+{
+    class Program
+    {
+        static void Plus<T>( T a, T b)
+        {
+            T c = (dynamic) a + b;
+            Console.WriteLine(c);
+
+        }
+        static void Main(string[] args)
+        {
+            
+            Plus("GENA", "_KIRILL");
+            Plus(6, 8);
+        }
+    }
+}
