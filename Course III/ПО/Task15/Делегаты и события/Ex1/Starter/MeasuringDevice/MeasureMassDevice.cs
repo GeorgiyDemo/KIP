2f4e2ce49cb92a9c9a5c8dbb250a27cb4f59e745
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-05-31 15:26:30 +0300

    ПО new tasks added

---
+++ Course III/ПО/Task15/Делегаты и события/Ex1/Starter/MeasuringDevice/MeasureMassDevice.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using DeviceControl;
+
+namespace MeasuringDevice
+{
+    public class MeasureMassDevice : MeasureDataDevice
+    {
+        /// <summary>
+        /// Construct a new instance of the MeasureMassDevice class.
+        /// </summary>
+        /// <param name="deviceUnits">Specifies the units used natively by the device.</param>
+        /// <param name="logFileName">Specifies the required file name used for logging in the class.</param>
+        public MeasureMassDevice(Units deviceUnits, string logFileName)
+        {
+            unitsToUse = deviceUnits;
+            measurementType = DeviceType.MASS;
+            loggingFileName = logFileName;
+        }
+
+        /// <summary>
+        /// Converts the raw data collected by the measuring device into a metric value.
+        /// </summary>
+        /// <returns>The latest measurement from the device converted to metric units.</returns>
+        public override decimal MetricValue()
+        {
+            decimal metricMostRecentMeasure;
+
+            if (unitsToUse == Units.Metric)
+            {
+                metricMostRecentMeasure = Convert.ToDecimal(mostRecentMeasure);
+            }
+            else
+            {
+                // Imperial measurements are in pounds.
+                // Multiply imperial measurement by 0.4536 to convert from pounds to kilograms. 
+                // Convert from an integer value to a decimal.
+                decimal decimalImperialValue = Convert.ToDecimal(mostRecentMeasure);
+                decimal conversionFactor = 0.4536M;
+                metricMostRecentMeasure = decimalImperialValue * conversionFactor;
+            }
+
+            return metricMostRecentMeasure;
+        }
+
+        /// <summary>
+        /// Converts the raw data collected by the measuring device into an imperial value.
+        /// </summary>
+        /// <returns>The latest measurement from the device converted to imperial units.</returns>
+        public override decimal ImperialValue()
+        {
+            decimal imperialMostRecentMeasure;
+
+            if (unitsToUse == Units.Imperial)
+            {
+                imperialMostRecentMeasure = Convert.ToDecimal(mostRecentMeasure);
+            }
+            else
+            {
+                // Metric measurements are in kilograms.
+                // Multiply metric measurement by 2.2046 to convert from kilograms to pounds. 
+                // Convert from an integer value to a decimal.
+                decimal decimalMetricValue = Convert.ToDecimal(mostRecentMeasure);
+                decimal conversionFactor = 2.2046M;
+                imperialMostRecentMeasure = decimalMetricValue * conversionFactor;
+            }
+
+            return imperialMostRecentMeasure;
+        }
+    }
+}
---
+++ Course III/ПО/Task15/Делегаты и события/Ex2/Starter/MeasuringDevice/IMeasuringDevice.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeasuringDevice
+{
+    public interface IMeasuringDevice
+    {
+        /// <summary>
+        /// Converts the raw data collected by the measuring device into a metric value.
+        /// </summary>
+        /// <returns>The latest measurement from the device converted to metric units.</returns>
+        decimal MetricValue();
+
+        /// <summary>
+        /// Converts the raw data collected by the measuring device into an imperial value.
+        /// </summary>
+        /// <returns>The latest measurement from the device converted to imperial units.</returns>
+        decimal ImperialValue();
+
+        /// <summary>
+        /// Starts the measuring device.
+        /// </summary>
+        void StartCollecting();
+
+        /// <summary>
+        /// Stops the measuring device. 
+        /// </summary>
+        void StopCollecting();
+
+        /// <summary>
+        /// Enables access to the raw data from the device in whatever units are native to the device.
+        /// </summary>
+        /// <returns>The raw data from the device in native format.</returns>
+        int[] GetRawData();
+
+        /// <summary>
+        /// Returns the file name of the logging file for the device.
+        /// </summary>
+        /// <returns>The file name of the logging file.</returns>
+        string GetLoggingFile();
+
+        /// <summary>
+        /// Gets the Units used natively by the device.
+        /// </summary>
+        Units UnitsToUse { get; }
+
+        /// <summary>
+        /// Gets an array of the measurements taken by the device.
+        /// </summary>
+        int[] DataCaptured { get; }
+
+        /// <summary>
+        /// Gets the most recent measurement taken by the device.
+        /// </summary>
+        int MostRecentMeasure { get; }
+
+        /// <summary>
+        /// Gets or sets the name of the logging file used. 
+        /// If the logging file changes this closes the current file and creates the new file.
+        /// </summary>
+        string LoggingFileName { get; set; }
+    }
+}
