5553f60226009eb5317127ec99619b6b48b79e86
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-09-07 17:46:09 +0300

    Конспект + фикс MainWindow

--- Course IV/ИСРПО/Project2/Project2/MainWindow.xaml.cs
+++ Course IV/ИСРПО/Project2/Project2/MainWindow.xaml.cs
@@ -27,48 +27,100 @@ namespace Project2
             InitializeComponent();
         }
 
-        static public double Checker(double x, double a)
+
+        static public String Checker(double x, double a)
         {
+            double y = 0;
+            double u = 0;
             if (x < 0)
-                return x * x * Math.Sin(x + a);
+            {
+                y = x * x * Math.Sin(x + a);
+            }
             else if (x >= 0)
-                return x / Math.Log(x + a);
-            return 0;
+            {
+                u = Math.Log(x + a);
+                if (u == 0)
+                    return "Деление на ноль";
+
+                 y = x / u;
+                if (Double.IsNaN(y))
+                {
+                    return "Нет решения функциии";
+                }
+               
+
+            }
+            return System.Convert.ToString(y);
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            double a = System.Convert.ToDouble(InputTextbox.Text);
 
-            string[] labels = new string[] { "Значение X", "Значение Y" };
-
-            foreach (string label in labels)
-            {
-                DataGridTextColumn column = new DataGridTextColumn();
-                column.Header = label;
-                column.Binding = new Binding(label.Replace(' ', '_'));
-
-                MainDataGrid.Columns.Add(column);
-            }
-
-            for (int i = -20; i < 21; i++)
-                if ((i >= -20) && (i <= 20))
-                {
-
-                    string[] values = new string[] { System.Convert.ToString(i), Convert.ToString(Math.Round(Checker(i, a), 4)) };
-
-                    dynamic row = new ExpandoObject();
-
-                    for (int ii = 0; ii < labels.Length; ii++)
-                        ((IDictionary<String, Object>)row)[labels[ii].Replace(' ', '_')] = values[ii];
-
-                    MainDataGrid.Items.Add(row);
-                }
         }
             
         private void MainDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        private void InputTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            try
+            {
+                try
+                {
+                    MainDataGrid.Columns.RemoveAt(1);
+                    MainDataGrid.Columns.RemoveAt(0);
+                    MainDataGrid.Items.Clear();
+                }
+                catch (Exception)
+                {
+
+                }
+
+                double a = System.Convert.ToDouble(InputTextbox.Text);
+                string[] labels = new string[] { "Значение X", "Значение Y" };
+
+                foreach (string label in labels)
+                {
+                    DataGridTextColumn column = new DataGridTextColumn();
+                    column.Header = label;
+                    column.Binding = new Binding(label.Replace(' ', '_'));
+
+                    MainDataGrid.Columns.Add(column);
+                }
+
+                for (int i = -20; i < 21; i++)
+                    if ((i >= -20) && (i <= 20))
+                    {
+
+                        string[] values = new string[] { System.Convert.ToString(i), Convert.ToString(Checker(i, a)) };
+
+                        dynamic row = new ExpandoObject();
+
+                        for (int ii = 0; ii < labels.Length; ii++)
+                            ((IDictionary<String, Object>)row)[labels[ii].Replace(' ', '_')] = values[ii];
+
+                        MainDataGrid.Items.Add(row);
+                    }
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Введено слишком большое число");
+                InputTextbox.Focus();
+            
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введено некорректное значение");
+                InputTextbox.Focus();
+            }
+            catch (OutOfMemoryException)
+            {
+                MessageBox.Show("Не хватает памяти для обработки введенного числа!");
+                InputTextbox.Focus();
+            }
         }
     }
 }
