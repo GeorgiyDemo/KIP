0aa91cabd38f082249ba93dc397ab568e059f114
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-09-10 15:06:17 +0300

    ИСРПО - Project3 added

---
+++ Course IV/ИСРПО/Project3/Project3/СylinderWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project3
+{
+    /// <summary>
+    /// Логика взаимодействия для СylinderWindow.xaml
+    /// </summary>
+    public partial class СylinderWindow : Window
+    {
+        public СylinderWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
