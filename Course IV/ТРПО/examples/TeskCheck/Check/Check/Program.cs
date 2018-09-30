18bbcb17ec3704d2603090444a67412b02e4e710
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-09-30 12:43:49 +0300

    ТРПО - examples

---
+++ Course IV/ТРПО/examples/TeskCheck/Check/Check/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Check
+{
+
+
+    class Employee{
+        public virtual string GetTypeName(){
+            return "THIS IS EMPLOYEW";
+
+        }
+    }
+    class Manager:Employee{
+        public override string GetTypeName()
+        {
+            return "THIS IS MANAGER";
+        }
+    }
+
+    class ManualWorker : Employee{
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee myEmaployee;
+            Manager myManager = new Manager();
+            ManualWorker myWorker = new ManualWorker();
+
+            myEmaployee = myManager;
+            Console.WriteLine(myEmaployee.GetTypeName());
+
+            myEmaployee = myWorker;
+            Console.WriteLine(myEmaployee.GetTypeName());
+        }
+    }
+}
---
+++ Course IV/ТРПО/examples/VirtualExample/VirtualExample/VirtualExample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace VirtualExample
+{
+
+
+    public class A
+    {
+       
+        public virtual double Func(double a)
+        {
+            return Math.Pow(a, 2);
+        }
+
+    }
+
+
+    public class B : A
+    {
+
+        public override double Func(double a)
+        {
+            return Math.Pow(a, 3);
+        }
+
+    }
+
+    public class C : B
+    {
+        public virtual double Func(double a)
+        {
+            return Math.Pow(a, 2);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            double a = System.Convert.ToDouble(Console.ReadLine());
+
+
+
+            A a_obj = new A();
+            Console.WriteLine("A: "+a_obj.Func(a));
+            B b_obj = new B();
+            Console.WriteLine("B: "+b_obj.Func(a));
+            C c_obj = new C();
+            Console.WriteLine("C: "+c_obj.Func(a));
+
+        }
+    }
+}
