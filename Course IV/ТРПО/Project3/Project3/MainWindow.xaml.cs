ffd54a41124f33d82ed1a541d4548c8e364ded2a
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-10-05 13:47:23 +0300

    Проект 3

---
+++ Course IV/ТРПО/Project3/Project3/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+
+        public char[,] GetArray()
+        {
+            char[,] array = new char[10, 10];
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    array[i, j] = (char)rnd.Next(65, 93);
+                }
+            }
+                ///
+                return array;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var array = GetArray();
+            var rows = array.GetLength(0);
+            var columns = array.GetLength(1);
+
+            var t = new DataTable();
+            for (var c = 0; c < columns; c++)
+                t.Columns.Add(new DataColumn(c.ToString()));
+
+             for (var r = 0; r < rows; r++)
+            {
+                var newRow = t.NewRow();
+                for (var c = 0; c < columns; c++)
+                {
+                    newRow[c] = array[r, c];
+                }
+                t.Rows.Add(newRow);
+            }
+            MainDataGrid.ItemsSource = t.DefaultView;
+        }
+
+    }
+}
