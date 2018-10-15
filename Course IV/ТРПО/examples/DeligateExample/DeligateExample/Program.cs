5673da1948a8dfd334aca50ab17e73de068477d4
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-10-15 16:05:09 +0300

    ТРПО - делегаты надо доделать по заданию

---
+++ Course IV/ТРПО/examples/DeligateExample/DeligateExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DeligateExample
+{
+    delegate void Del1();
+    delegate void Del2(int a);
+
+    class Program
+    {
+        static void summ(int a)
+        {
+            Console.WriteLine("Сумма = {0}", a + a);
+        }
+
+        static void summ2(int a)
+        {
+            Console.WriteLine("Сумма2 = {0}", a + a + 1);
+        }
+
+        static void summ3(int a)
+        {
+            Console.WriteLine("Сумма3 = {0}", a + a + 2);
+        }
+
+        static void Main()
+        {
+            //Del1 de1 = new Del1();
+
+            Del2 de2 = null;
+            de2 += summ;
+            de2 += summ2;
+            de2 += summ3;
+            de2(1);
+        }
+    }
+}
