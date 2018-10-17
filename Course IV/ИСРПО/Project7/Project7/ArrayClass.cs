bf8360b5fc1e5f371471af19c56992439ce85c93
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-10-17 17:39:47 +0300

    ИСРПО Project7 added (снова DataGrid на WPF)

---
+++ Course IV/ИСРПО/Project7/Project7/ArrayClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project7
+{
+    class ArrayClass
+    {
+        private static Random random = new Random();
+        public int[,] GetArray()
+        {
+            int[,] array = new int[10, 10];
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                 
+            array[i, j] = random.Next(0, 100);
+            return array;
+        }
+
+
+        public DataTable DataGridInput()
+        {
+            int[,] MainArray = GetArray();
+            var rows = MainArray.GetLength(0);
+            var columns = MainArray.GetLength(1);
+
+            var t = new DataTable();
+            for (var c = 0; c < columns; c++)
+                t.Columns.Add(new DataColumn(c.ToString()));
+
+            for (var r = 0; r < rows; r++)
+            {
+                var newRow = t.NewRow();
+                for (var c = 0; c < columns; c++)
+                    newRow[c] = MainArray[r, c];
+                t.Rows.Add(newRow);
+            }
+
+            return t;
+        }
+    }
+
+}
---
+++ Course IV/ИСРПО/Project7/Project7/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+
+namespace Project7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void DataGrid_Initialized(object sender, EventArgs e)
+        {
+            ArrayClass obj = new ArrayClass();
+            MainDataGrid.ItemsSource = obj.DataGridInput();
+
+        }
+    }
+}
