0019026a39e009b4bc105a0be871b38eadc1d98c
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-10-19 14:00:59 +0300

    Сайтостроение - Задание 4  - работа с PHP

--- Course IV/Сайтостроение/Козлобаев/Site4/action.php
+++ Course IV/Сайтостроение/Козлобаев/Site4/action.php
@@ -1,2 +1,12 @@
-<?php echo htmlspecialchars($_POST['name']); ?>
-<?php echo htmlspecialchars($_POST['age']); ?>
+<?php
+
+    print("<h3>Контактная информация</h3>Имя: ".$_POST['name']."<br>");
+    print("Телефон: ".$_POST['phone']."<br>"); 
+    print("E-mail: ".$_POST['email']."<br>");
+    print("Дата посещения: ".$_POST['date']."<br>");
+
+    print("<h3>Персональная информация</h3>Возраст: ".$_POST['age']."<br>");
+    print("<br><br>");
+    print("<h3>Оценка нашего заведения</h3>Почему вы выбрали наше заведение?: ".$_POST['radiochoise_type']."<br>");
+    print("Вы будете рекомендовать наше заведение своим знакомым?: ".$_POST['radiochoise_advice']."<br>");
+?>
