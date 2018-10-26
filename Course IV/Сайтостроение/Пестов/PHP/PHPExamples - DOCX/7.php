3eb6a4824fbcdac992195a03e3f8bf9cbc506728
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-10-26 15:36:50 +0300

    Сайтостроение Пестов - допилил PHP

--- Course IV/Сайтостроение/Пестов/PHP/PHPExamples - DOCX/7.php
+++ Course IV/Сайтостроение/Пестов/PHP/PHPExamples - DOCX/7.php
@@ -1,32 +1,28 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<title>Задание 7</title>
-</head>
-<body>
-	<?php 
-	    $a = strval($_POST["number"]);
-		$a = (string) round(($a * 12/360), 2);
-		$hour = str_split($a);
-		$max = count($hour);
-		$i = 0;
-		$min = 0;
-		while($i <= $max-1)
+<?php 
+	/*
+	Разработайте программу, которая определяла количество прошедших часов по введенным пользователем градусах.
+	Введенное число может быть от 0 до 360.
+	*/
+	
+	$a = strval($_POST["number"]);
+	$a = (string) round(($a * 12/360), 2);
+	$hour = str_split($a);
+	$max = count($hour);
+	$i = 0;
+	$min = 0;
+	while($i <= $max-1)
+	{
+		if($i == ($max - 2) || $i == ($max - 1))
 		{
-			if($i == ($max - 2) || $i == ($max - 1))
+			if($hour[$i] == 0)
 			{
-				if($hour[$i] == 0)
-				{
-					$hour[$i] = '0';
-				}
-				$min =$min.$hour[$i];
+				$hour[$i] = '0';
 			}
-			$i++;
+			$min =$min.$hour[$i];
 		}
-		$min = round($min * 0.6);
-		echo "<br/>";
-		echo $hour[0].$hour[1]." ч ".$min." мин";
-	?>
-</body>
-</html>
+		$i++;
+	}
+	$min = round($min * 0.6);
+	echo "<br/>";
+	echo $hour[0].$hour[1]." ч ".$min." мин";
+?>
