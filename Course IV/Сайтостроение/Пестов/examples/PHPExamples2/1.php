4d57a52ec9281190c9f1a7154b6cadddb4d05a96
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-10-26 11:50:10 +0300

    Сайтостроение Пестов - реструктуризация

--- Course IV/Сайтостроение/Пестов/examples/PHPExamples2/1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$thisnumber = strval($_POST["number"]);
-$localeresult = 0;
-for ($i=0;$i<strlen($thisnumber);$i++)
-{
-    $localeresult += intval($thisnumber[$i]);
-}
-print("Результат: ".$localeresult);
-?>
--- Course IV/Сайтостроение/Пестов/examples/PHPExamples2/2.php
+++
@@ -1,5 +0,0 @@
-<?php
-$int = strval($_POST["count_number"]);
-$thisnumber = strval($_POST["number"]);
-echo "Цифра ".$int." в числе ".$thisnumber." встречается ".substr_count($thisnumber, $int)." раза";
-?>
