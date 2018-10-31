8643ab6b3fb974ff642f56bfc9f4221b2f7f8ccb
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-10-31 20:45:05 +0300

    ТРПО - Работа с WinForms (ужс)

---
+++ Course IV/ТРПО/examples/MenuDialogExample/MenuDialogExample/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace MenuDialogExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.рисункиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.рисункиИзСпискаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.водоемыРоссииToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.рекиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.волгаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.москваToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.окаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.моряToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.черноеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.балтийскоеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.азовскоеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.озераToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ладожскоеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.онежскоеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.байкалToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.SvaeButton = new System.Windows.Forms.Button();
+            this.MainPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.рисункиToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // рисункиToolStripMenuItem
+            // 
+            this.рисункиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.рисункиИзСпискаToolStripMenuItem});
+            this.рисункиToolStripMenuItem.Name = "рисункиToolStripMenuItem";
+            this.рисункиToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.рисункиToolStripMenuItem.Text = "Рисунки";
+            // 
+            // рисункиИзСпискаToolStripMenuItem
+            // 
+            this.рисункиИзСпискаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.водоемыРоссииToolStripMenuItem});
+            this.рисункиИзСпискаToolStripMenuItem.Name = "рисункиИзСпискаToolStripMenuItem";
+            this.рисункиИзСпискаToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.рисункиИзСпискаToolStripMenuItem.Text = "Рисунки из списка";
+            // 
+            // водоемыРоссииToolStripMenuItem
+            // 
+            this.водоемыРоссииToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.рекиToolStripMenuItem,
+            this.моряToolStripMenuItem,
+            this.озераToolStripMenuItem});
+            this.водоемыРоссииToolStripMenuItem.Name = "водоемыРоссииToolStripMenuItem";
+            this.водоемыРоссииToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.водоемыРоссииToolStripMenuItem.Text = "Водоемы России";
+            // 
+            // рекиToolStripMenuItem
+            // 
+            this.рекиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.волгаToolStripMenuItem,
+            this.москваToolStripMenuItem,
+            this.окаToolStripMenuItem});
+            this.рекиToolStripMenuItem.Name = "рекиToolStripMenuItem";
+            this.рекиToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.рекиToolStripMenuItem.Text = "Реки";
+            // 
+            // волгаToolStripMenuItem
+            // 
+            this.волгаToolStripMenuItem.Name = "волгаToolStripMenuItem";
+            this.волгаToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.волгаToolStripMenuItem.Text = "Волга";
+            // 
+            // москваToolStripMenuItem
+            // 
+            this.москваToolStripMenuItem.Name = "москваToolStripMenuItem";
+            this.москваToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.москваToolStripMenuItem.Text = "Москва";
+            // 
+            // окаToolStripMenuItem
+            // 
+            this.окаToolStripMenuItem.Name = "окаToolStripMenuItem";
+            this.окаToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.окаToolStripMenuItem.Text = "Ока";
+            // 
+            // моряToolStripMenuItem
+            // 
+            this.моряToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.черноеToolStripMenuItem,
+            this.балтийскоеToolStripMenuItem,
+            this.азовскоеToolStripMenuItem});
+            this.моряToolStripMenuItem.Name = "моряToolStripMenuItem";
+            this.моряToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.моряToolStripMenuItem.Text = "Моря";
+            // 
+            // черноеToolStripMenuItem
+            // 
+            this.черноеToolStripMenuItem.Name = "черноеToolStripMenuItem";
+            this.черноеToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.черноеToolStripMenuItem.Text = "Черное";
+            // 
+            // балтийскоеToolStripMenuItem
+            // 
+            this.балтийскоеToolStripMenuItem.Name = "балтийскоеToolStripMenuItem";
+            this.балтийскоеToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.балтийскоеToolStripMenuItem.Text = "Балтийское";
+            // 
+            // азовскоеToolStripMenuItem
+            // 
+            this.азовскоеToolStripMenuItem.Name = "азовскоеToolStripMenuItem";
+            this.азовскоеToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.азовскоеToolStripMenuItem.Text = "Азовское";
+            // 
+            // озераToolStripMenuItem
+            // 
+            this.озераToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ладожскоеToolStripMenuItem,
+            this.онежскоеToolStripMenuItem,
+            this.байкалToolStripMenuItem});
+            this.озераToolStripMenuItem.Name = "озераToolStripMenuItem";
+            this.озераToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.озераToolStripMenuItem.Text = "Озера";
+            // 
+            // ладожскоеToolStripMenuItem
+            // 
+            this.ладожскоеToolStripMenuItem.Name = "ладожскоеToolStripMenuItem";
+            this.ладожскоеToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.ладожскоеToolStripMenuItem.Text = "Ладожское";
+            // 
+            // онежскоеToolStripMenuItem
+            // 
+            this.онежскоеToolStripMenuItem.Name = "онежскоеToolStripMenuItem";
+            this.онежскоеToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.онежскоеToolStripMenuItem.Text = "Онежское";
+            // 
+            // байкалToolStripMenuItem
+            // 
+            this.байкалToolStripMenuItem.Name = "байкалToolStripMenuItem";
+            this.байкалToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.байкалToolStripMenuItem.Text = "Байкал";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(135, 338);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(184, 48);
+            this.LoadButton.TabIndex = 1;
+            this.LoadButton.Text = "Загрузка изображения";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // SvaeButton
+            // 
+            this.SvaeButton.Location = new System.Drawing.Point(414, 338);
+            this.SvaeButton.Name = "SvaeButton";
+            this.SvaeButton.Size = new System.Drawing.Size(184, 48);
+            this.SvaeButton.TabIndex = 2;
+            this.SvaeButton.Text = "Сохранить файл";
+            this.SvaeButton.UseVisualStyleBackColor = true;
+            this.SvaeButton.Click += new System.EventHandler(this.SvaeButton_Click);
+            // 
+            // MainPictureBox
+            // 
+            this.MainPictureBox.Location = new System.Drawing.Point(92, 88);
+            this.MainPictureBox.Name = "MainPictureBox";
+            this.MainPictureBox.Size = new System.Drawing.Size(576, 234);
+            this.MainPictureBox.TabIndex = 3;
+            this.MainPictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Выбран пдпункт главного меню:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MainPictureBox);
+            this.Controls.Add(this.SvaeButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem рисункиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem рисункиИзСпискаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem водоемыРоссииToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem рекиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem волгаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem москваToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem окаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem моряToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem черноеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem балтийскоеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem азовскоеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem озераToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ладожскоеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem онежскоеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem байкалToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button SvaeButton;
+        private System.Windows.Forms.PictureBox MainPictureBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
