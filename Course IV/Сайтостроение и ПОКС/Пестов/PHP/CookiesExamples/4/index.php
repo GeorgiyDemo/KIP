9855f07fd20c744f02f05a6d81bc87e5c81c261e
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-11-02 13:26:30 +0300

    Пилим

--- Course IV/Сайтостроение и ПОКС/Пестов/PHP/CookiesExamples/4/index.php
+++ Course IV/Сайтостроение и ПОКС/Пестов/PHP/CookiesExamples/4/index.php
@@ -1,21 +1,26 @@
 <?php
+$catsmoney = array("cat1"=>12000, "cat2"=>1000, "cat3"=>3000);
+
 print(
  "<form action='index.php' method='post'>
- <legend>Ввод данных пользователя:</legend>
+ <legend>Выберите котика:</legend>
  <table>
    <tr> 
-     <td><label>Город:</label></td><td><input type='text' name='city' /></td>
+     <td><label>Котик №1:</label></td><td><img src='../../../img/cat1.jpg'><br>Цена: ".$catsmoney["cat1"]." руб. <br><input type='submit' name='cat1' value='В корзину'></td>
    </tr>
    <tr>
-   <td><label>Возраст:</label></td><td><input type='text' name='years' /></td>
+   <td><label>Котик №2:</label></td><td><img src='../../../img/cat2.jpg'><br>Цена: ".$catsmoney["cat2"]." руб. <br><input type='submit' name='cat2' value='В корзину'></td>
+   </tr>
+   <tr>
+   <td><label>Котик №3:</label></td><td><img src='../../../img/cat3.jpg'><br>Цена: ".$catsmoney["cat3"]." руб. <br><input type='submit' name='cat3' value='В корзину'></td>
    </tr>
  </table>
- <input type='submit' name='send' value='Отправить'>
+ <input type='submit' name='send' value='В корзину'>
 </form>".
 "<form action='output.php'>
-<input type='submit' value='Выполнить задание'>
 </form>"
 );
+
 if (($_POST["city"] != Null) && ($_POST["years"] != Null))
     SetCookie("city",$_POST["city"],time()+3600);
     SetCookie("years",$_POST["years"],time()+3600);
