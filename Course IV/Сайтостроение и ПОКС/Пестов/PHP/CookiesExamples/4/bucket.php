59e87e3531cdbbbd6888d5bb59813efb0c7be2fe
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-11-07 14:14:04 +0300

    Сайтостроение PHP -  корзина

--- Course IV/Сайтостроение и ПОКС/Пестов/PHP/CookiesExamples/4/bucket.php
+++ Course IV/Сайтостроение и ПОКС/Пестов/PHP/CookiesExamples/4/bucket.php
@@ -1,19 +1,53 @@
 <?php
+print("  <style type='text/css'>
+.layout {
+ width: 100%;
+}
+TD {
+ vertical-align: top;
+ padding: 5px;
+}
+TD.leftcol {
+ width: 200px;
+ background: #ccc;
+}
+TD.rightcol {
+ background: #fc3;
+}
+</style>");
+
+$catsnamesarr = array("cat1"=>"Котик №1", "cat2"=>"Котик №2", "cat3"=>"Котик №3", "cat4"=>"Котик №4", "cat5"=>"Котик №5","cat6"=>"Котик №6");
+$catspricearr = array("cat1"=>12000, "cat2"=>1000, "cat3"=>3000, "cat4"=>2000, "cat5"=>1900, "cat6"=>2500);
+print("
+ <form>
+ <legend>Ваша корзина:</legend>
+ <table cellspacing='0' class='layout'>
+ <tr> 
+  <td class='leftcol'><b>Название товара</b></td>
+  <td class='rightcol'><b>Цена<b>
+  </td>
+ </tr>
+ 
+");
+  $finalprice = 0;
+  foreach($_COOKIE as $key=>$value)
+  {
+    print(" <tr> 
+    <td class='leftcol'>".$catsnamesarr[$key]."</td>
+    <td class='rightcol'>".$catspricearr[$key]." руб."."</td>
+   </tr>");
+   $finalprice +=(double) $catspricearr[$key];
+  }
+
 print(
- "<form>
- <legend>Результат выполнения задания:</legend>
- <table>
-  <tr> 
-  <td><label>Имя:</label></td><td><input type='text' name='name'/></td>
-  </tr>
-   <tr> 
-     <td><label>Город:</label></td><td><input type='text' name='city' value='".$_COOKIE["city"]."' /></td>
-   </tr>
-   <tr>
-   <td><label>Возраст:</label></td><td><input type='text' name='years' value='".$_COOKIE["years"]."' /></td>
-   </tr>
+  "<tr> 
+  <td class='leftcol'><b>Итог:</b></td>
+  <td class='rightcol'><b>".$finalprice." руб.<b></td>
+ </tr>
  </table>
- <input type='submit' name='send' value='Отправить'>
-</form>"
-);
+ </form>
+ <form action='index.php' method='post'>
+ <input type='submit' name='send' value='К списку товаров'>
+</form>");
+
 ?>
--- Course IV/Сайтостроение и ПОКС/Пестов/Site1/php/cookies/4/index.php
+++ Course IV/Сайтостроение и ПОКС/Пестов/Site1/php/cookies/4/index.php
@@ -1,27 +1,36 @@
 <?php
-$catsmoney = array("cat1"=>12000, "cat2"=>1000, "cat3"=>3000);
+$catsmoney = array(1=>12000, 2=>1000, 3=>3000, 4=>2000, 5=>1900, 6=>2500);
 
 print(
  "<form action='index.php' method='post'>
  <legend>Выберите котика:</legend>
- <table>
-   <tr> 
-     <td><label>Котик №1:</label></td><td><img src='../../../img/cat1.jpg'><br>Цена: ".$catsmoney["cat1"]." руб. <br><input type='submit' name='cat1' value='В корзину'></td>
-   </tr>
-   <tr>
-   <td><label>Котик №2:</label></td><td><img src='../../../img/cat2.jpg'><br>Цена: ".$catsmoney["cat2"]." руб. <br><input type='submit' name='cat2' value='В корзину'></td>
-   </tr>
-   <tr>
-   <td><label>Котик №3:</label></td><td><img src='../../../img/cat3.jpg'><br>Цена: ".$catsmoney["cat3"]." руб. <br><input type='submit' name='cat3' value='В корзину'></td>
-   </tr>
- </table>
- <input type='submit' name='send' value='В корзину'>
-</form>".
-"<form action='output.php'>
+ <table>");
+
+  for ($i=1;$i<7;$i++)
+  {
+    print("<tr> 
+    <td><label>Котик №".$i.":</label></td><td><img src='../../../img/cat".$i.".jpg'><br>Цена: ".$catsmoney[$i]." руб. <br><input type='submit' name='cat".$i."' value='В корзину'></td>
+  </tr>");
+  }
+
+  print(
+ "</table>
+</form>
+<form action='output.php'>
 </form>"
 );
 
-if (($_POST["city"] != Null) && ($_POST["years"] != Null))
-    SetCookie("city",$_POST["city"],time()+3600);
-    SetCookie("years",$_POST["years"],time()+3600);
+
+
+foreach($_POST as $key=>$value)
+  if (substr($key, 0, 3)=='cat')
+    SetCookie($key, $value, time() + 3600);
+
+print(
+ "<form action='bucket.php' method='post'>".
+ "<input type='hidden' name='catsprice' value=".htmlentities(serialize($catsmoney)).">".
+ "<input type='submit' name='send' value='Переход к корзине'>
+</form>"
+);
+
 ?>
