7bc842093049da424a9acc7b28729257857f4c81
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-11-08 10:54:58 +0300

    ТРПО - сделана вся практика, кроме работы с БД

---
+++ Course IV/ТРПО/Practs/6/GraphicEditorExample/GraphicEditorExample/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace GraphicEditorExample
+{
+    public partial class Form1 : Form
+    {
+        bool drawing; GraphicsPath currentPath; Point oldLocation; Pen currentPen;
+
+        public Form1()
+        {
+            InitializeComponent();
+            drawing = false;
+            currentPen = new Pen(Color.Black);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picDrawingSurface_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (picDrawingSurface.Image == null)
+            {
+                MessageBox.Show("Сначала создайте новый файл!");
+                return;
+            }
+            if (e.Button == MouseButtons.Left)
+            {
+                drawing = true;
+                oldLocation = e.Location;
+                currentPath = new GraphicsPath();
+            }
+        }
+
+        private void picDrawingSurface_MouseUp(object sender, MouseEventArgs e)
+        {
+            drawing = false;
+            try
+            {
+                currentPath.Dispose();
+            }
+
+            catch
+            {
+            };
+        }
+
+
+        private void picDrawingSurface_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawing) {
+                Graphics g = Graphics.FromImage(picDrawingSurface.Image);
+                currentPath.AddLine(oldLocation, e.Location);
+                g.DrawPath(currentPen, currentPath);
+                oldLocation = e.Location; g.Dispose();
+                picDrawingSurface.Invalidate();
+            }
+        }
+        private void новыйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (picDrawingSurface.Image != null)
+            {
+                var result = MessageBox.Show("Сохранить текущее изображение перед созданием нового рисунка?", "Предупреждение", MessageBoxButtons.YesNoCancel);
+                switch (result)
+                {
+                    case DialogResult.No:
+                        break;
+                    case DialogResult.Yes:
+                        сохранитьToolStripMenuItem_Click(sender, e);
+                        break;
+                    case DialogResult.Cancel:
+                        return;
+                }
+            }
+            Bitmap pic = new Bitmap(614, 300);
+            picDrawingSurface.Image = pic;
+            Graphics grf = Graphics.FromImage(pic);
+            grf.Clear(Color.White);
+            picDrawingSurface.Image = pic;
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveDlg = new SaveFileDialog();
+            SaveDlg.Filter = "JPEG Image|*.jpg|Bitmap Image|*.bmp|GIF Image|*.gif|PNG Image|*.png";
+            SaveDlg.Title = "Save an Image File";
+            SaveDlg.FilterIndex = 4;
+            SaveDlg.ShowDialog();
+
+
+            if (SaveDlg.FileName != "")
+
+                { System.IO.FileStream fs = (System.IO.FileStream)SaveDlg.OpenFile();
+                    switch (SaveDlg.FilterIndex)
+                    { case 1: this.picDrawingSurface.Image.Save(fs, ImageFormat.Jpeg);
+                            break;
+                      case 2: this.picDrawingSurface.Image.Save(fs, ImageFormat.Bmp);
+                            break;
+                      case 3: this.picDrawingSurface.Image.Save(fs, ImageFormat.Gif);
+                            break;
+                      case 4: this.picDrawingSurface.Image.Save(fs, ImageFormat.Png);
+                            break;
+                    }
+                    fs.Close();
+            }
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OP = new OpenFileDialog();
+            OP.Filter = "JPEG Image|*.jpg|Bitmap Image|*.bmp|GIF Image|*.gif|PNG Image|*.png";
+            OP.Title = "Open an Image File";
+            OP.FilterIndex = 1;
+            if (OP.ShowDialog() != DialogResult.Cancel)
+                picDrawingSurface.Load(OP.FileName);
+            picDrawingSurface.AutoSize = true;
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            новыйToolStripMenuItem_Click( sender, e);
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            сохранитьToolStripMenuItem_Click(sender, e);
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            открытьToolStripMenuItem_Click(sender, e);
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    } 
+}
