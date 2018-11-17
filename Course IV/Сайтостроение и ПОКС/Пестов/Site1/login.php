29b6de31e90d77cbe0f47788e84edc0b969f0c54
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-11-17 15:33:11 +0300

    Сайтостроение Пестов - готово

--- Course IV/Сайтостроение и ПОКС/Пестов/Site1/login.php
+++ Course IV/Сайтостроение и ПОКС/Пестов/Site1/login.php
@@ -32,7 +32,7 @@
 		</style>
 
 			<?php
-				if (isset($_COOKIE['registrated']))
+				if ((isset($_COOKIE['registrated'])) || ($_POST["setlogin"]=="booleantrue"))
 				{
 					print("
 					<ul class='nav nav-pills'>
@@ -57,10 +57,13 @@
 						</div>
 						<button class='btn btn-lg btn-primary btn-block' type='submit'>Войти</button>
 					</form>
+					<form class='formreg' action='login.php' method='POST'>
+					<button class='btn' name='setregistration' value='booleantrue' type='submit'>Необходимо зарегистрироваться?</button>
+					</form>
 				 </div>
 					");
 				}
-				
+				//ФИЛЬТРАЦИЯ
 				else
 				{
 					print("
@@ -78,16 +81,19 @@
 					<div class='container'>
 					<form class='form-signin' action='./php/loginlogic/registration.php' method='POST'>
 						<h2 class='form-signin-heading'>Регистрация</h2>
-						<label for='inputName' class='sr-only'>Имя пользователя:</label>
-						<input type='text' id='inputName' class='form-control' name='name' placeholder='Имя пользователя:' required autofocus>
+						<label for='inputName' class='sr-only'>Имя пользователя</label>
+						<input type='text' id='inputName' class='form-control' name='name' placeholder='Имя пользователя' required autofocus>
 						<label for='inputEmail' class='sr-only'>Адрес e-mail</label>
 						<input type='email' id='inputEmail' class='form-control' name='login' placeholder='Адрес e-mail' required autofocus>
 						<label for='inputPassword' class='sr-only'>Пароль</label>
 						<input type='password' id='inputPassword' class='form-control' name='pass' placeholder='Пароль' required>
 						<div class='checkbox'>
 						</div>
-						<button class='btn btn-lg btn-primary btn-block' type='submit'>Войти</button>
-					</form>
+						<button class='btn btn-lg btn-primary btn-block' type='submit'>Зарегистрироваться</button>
+						</form>
+						<form class='formlog' action='login.php' method='POST'>
+						<button class='btn' name='setlogin' value='booleantrue' type='submit'>Уже зарегистрированы?</button>
+						</form>
 				 </div>
 					");
 				}
