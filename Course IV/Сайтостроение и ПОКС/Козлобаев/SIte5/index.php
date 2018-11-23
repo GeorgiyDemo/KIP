bded6d2c1f5c353bba10a605d1ae065006d0c787
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-11-23 20:48:48 +0300

    Козлобаев - готовое 5 задание (теперь точно)

--- Course IV/Сайтостроение и ПОКС/Козлобаев/SIte5/index.php
+++ Course IV/Сайтостроение и ПОКС/Козлобаев/SIte5/index.php
@@ -1,66 +1,33 @@
 <?php
-print("MEOW");
-/*
-print("  <style type='text/css'>
-.layout {
- width: 100%;
-}
-TD {
- vertical-align: top;
- padding: 5px;
-}
-TD.leftcol {
- width: 200px;
- background: #ccc;
-}
-TD.rightcol {
- background: #fc3;
-}
-</style>");
 
+  print("<title>HTML</title>
+  <meta charset='utf-8' />
+  <link rel='stylesheet' type='text/css' href='style.css'>");
 
-$link = mysqli_connect("localhost", "root", "root", "Pract5");
-    $checkdublicate = mysqli_query($link, "SELECT * FROM Pract5.'Table';");
-    if ($checkdublicate->num_rows == 0)
-    {
-        var_dump(fetch_assoc());
-        $finalresult->fetch_assoc();
-        foreach($finalresult as $key=>$value)
-            print($key." ".$value);
-    }
-
-print("
- <form>
- <legend>Таблица:</legend>
- <table cellspacing='0' class='layout'>
- <tr> 
-  <td class='leftcol'><b>Название товара</b></td>
-  <td class='rightcol'><b>Цена<b>
-  </td>
- </tr>
-  </form>
-");
-/*
-  $finalprice = 0;
-  foreach($_COOKIE as $key=>$value)
-  {
-    print(" <tr> 
-    <td class='leftcol'>".$catsnamesarr[$key]."</td>
-    <td class='rightcol'>".$catspricearr[$key]." руб."."</td>
-   </tr>");
-   $finalprice +=(double) $catspricearr[$key];
+  $link = mysqli_connect("localhost", "root", "root", "Pract5");
+  if (mysqli_connect_errno()) {
+      printf("Не удалось подключиться: %s\n", mysqli_connect_error());
+      exit();
   }
 
-print(
-  "<tr> 
-  <td class='leftcol'><b>Итог:</b></td>
-  <td class='rightcol'><b>".$finalprice." руб.<b></td>
- </tr>
- </table>
- </form>
- <form action='index.php' method='post'>
- <input type='submit' name='send' value='К списку товаров'>
-</form>");
+  $sql = 'SHOW COLUMNS FROM ThisTable';
+  $res = $link->query($sql);
 
-*/
+  print("<table> 
+  <tr id='header'>");
+
+  while($row = $res->fetch_assoc()){
+    print("<th>".$row['Field']."</th>");
+  }
+  print("</tr>");
+  if ($result = mysqli_query($link, "SELECT * FROM ThisTable "))
+  {
+
+      while($stringresult = $result->fetch_array())
+        print("<tr><td>".$stringresult['id']."</td><td>".$stringresult['name']."</td><td>".$stringresult['phone']."</td><td>".$stringresult['address']."</td></tr>");
+      mysqli_free_result($result);
+  }
+  print("</table>");
+  mysqli_close($link);
+
 ?>
