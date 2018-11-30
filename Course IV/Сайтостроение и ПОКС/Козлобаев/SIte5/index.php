b1106e5847aae0afd6f6e8e550c1e9de2aaa5a5d
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-11-30 11:59:13 +0300

    Сайтостроение - адаптация задания под условие v2

--- Course IV/Сайтостроение и ПОКС/Козлобаев/SIte5/index.php
+++ Course IV/Сайтостроение и ПОКС/Козлобаев/SIte5/index.php
@@ -10,48 +10,26 @@
       exit();
   }
 
-  $sql = 'SHOW COLUMNS FROM SS_Products';
-  $res = $link->query($sql);
-
   print("<table> 
   <tr id='header'>");
 
-  while($row = $res->fetch_assoc()){
-    print("<th>".$row['Field']."</th>");
-  }
+    print("<th>productID</th>".
+    "<th>categoryID</th>".
+    "<th>name</th>".
+    "<th>customers_rating</th>".
+    "<th>Price</th>"
+  );
+
   print("</tr>");
-  if ($result = mysqli_query($link, "SELECT * FROM SS_Products LIMIT 50"))
+  if ($result = mysqli_query($link, "SELECT * FROM SS_Products LIMIT 150"))
   {
 
       while($stringresult = $result->fetch_array())
         print("<tr><td>".$stringresult['productID'].
               "</td><td>".$stringresult['categoryID'].
               "</td><td>".$stringresult['name'].
-              "</td><td>".$stringresult['description'].
               "</td><td>".$stringresult['customers_rating'].
               "</td><td>".$stringresult['Price'].
-              "</td><td>".$stringresult['Price'].
-              "</td><td>".$stringresult['in_stock'].
-              "</td><td>".$stringresult['customer_votes'].
-              "</td><td>".$stringresult['items_sold'].
-              "</td><td>".$stringresult['enabled'].
-              "</td><td>".$stringresult['list_price'].
-              "</td><td>".$stringresult['product_code'].
-              "</td><td>".$stringresult['sort_order'].
-              "</td><td>".$stringresult['default_picture'].
-              "</td><td>".$stringresult['date_added'].
-              "</td><td>".$stringresult['date_modified'].
-              "</td><td>".$stringresult['viewed_times'].
-              "</td><td>".$stringresult['eproduct_filename'].
-              "</td><td>".$stringresult['eproduct_available_days'].
-              "</td><td>".$stringresult['eproduct_download_times'].
-              "</td><td>".$stringresult['weight'].
-              "</td><td>".$stringresult['meta_description'].
-              "</td><td>".$stringresult['meta_keywords'].
-              "</td><td>".$stringresult['free_shipping'].
-              "</td><td>".$stringresult['min_order_amount'].
-              "</td><td>".$stringresult['shipping_freight'].
-              "</td><td>".$stringresult['classID'].
               "</td>"
               
             );
