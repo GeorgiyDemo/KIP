fd0b7bf88c2fb76b5f6ea762ef8601d1893e44bf
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-12-19 12:42:22 +0300

    Сайтостроение Пестов - структуризация и оформление исходников по игре с алкоголиками

--- Course IV/Сайтостроение и ПОКС/Пестов/Site1/AlcoholicsGame/fight.php
+++ Course IV/Сайтостроение и ПОКС/Пестов/Site1/AlcoholicsGame/fight.php
@@ -1,37 +1,29 @@
+<?php
+    session_start();
+    $hp_serv = 10-(int)$_SESSION['hp_serv'];
+    $hp_user = 10- (int)$_SESSION['hp_user'];
+    $name=$_SESSION['username'];
+    $error=(int)$_SESSION['error_w'];
 
- <?php
-     session_start();
-$hp_serv =10-(int)$_SESSION['hp_serv'];
-$hp_user=10- (int)$_SESSION['hp_user'];
-$name=$_SESSION['username'];
-$error=(int)$_SESSION['error_w'];
-if($error==1)
-    print('Ошибка ввода, повторите попытку!'.'<br><br>');
-
-print('HP алкоголик <b>'.$name.'</b> => '.$hp_user."<br>");
-print('HP алкоголик <b>Вася</b> => '.$hp_serv."<br><br>");
-
+    if ($error==1)
+        print('Ошибка ввода, повторите попытку!'.'<br><br>');
+ 
+    print('HP алкоголик <b>'.$name.'</b> => '.$hp_user."<br>");
+    print('HP алкоголик <b>Вася</b> => '.$hp_serv."<br><br>");
 ?>
 
 <html>
-    
    <head>
         <meta charset="UTF-8">
         <title>Бой</title>
-    </head>  
-    
-   
-       
-        <form name="ataka" action="act_fight.php" method="POST">
+    </head>
+    </body>
+        <form action="act_fight.php" method="POST">
             <input type="text" name="point" placeholder="Число от 1 до 3" ><br/>
             <input type="submit" name="vidpr" value="Отправить"><br/>
-  
         </form>
-
-      
-
      </body>
 
 </html>
- <body>
+
     
--- Course IV/Сайтостроение и ПОКС/Пестов/Site1/AlcoholicsGame/s_fight.php
+++ Course IV/Сайтостроение и ПОКС/Пестов/Site1/AlcoholicsGame/s_fight.php
@@ -1,44 +1,34 @@
-
-<html>
-    
+<html>    
    <head>
         <meta charset="UTF-8">
         <title>Конец игры</title>
     </head>  
-    
     <body>
-        
-        <form name="gameover" action="index.php" method="POST">
-            
+        <form name="gameover" action="index.php" method="POST">   
             <input type="submit" name="ank_submit" value="В начало"><br/>
-  
         </form>
-        
-      
-        
      </body>
-
 </html>
+
 <?php
-session_start();
-$_SESSION['hp_serv']=0; 
-$_SESSION['hp_user']=0;  
-echo "Вы умерли!";
-$filename="textfile/fight.txt";
-$buffer="";
-$arr_m="";
-echo '<p>';
-if (file_exists($filename))
+    session_start();
+    $_SESSION['hp_serv']=0; 
+    $_SESSION['hp_user']=0;
+    print("Вы умерли!");
+    $filename="textfile/fight.txt";
+    $buffer="";
+    $arr_m="";
+    print('<p>');
+    
+    if (file_exists($filename))
     {
-     $file_m = fopen($filename,"r");
-    while($buffer=fgets($file_m))
-            
-    {
-  $arr_m .=$buffer."<br/>";
+        $file_m = fopen($filename,"r");
+        while($buffer=fgets($file_m))
+            $arr_m .=$buffer."<br/>";
+        print($arr_m ."<br/>");
+        fclose($file_m);
     }
-       
- echo $arr_m ."<br/>";
-    fclose($file_m);   }
-   $file_m = fopen($filename,"w");
-   fclose($file_m);
-session_abort();
+    $file_m = fopen($filename,"w");
+    fclose($file_m);
+    session_abort();
+?>
