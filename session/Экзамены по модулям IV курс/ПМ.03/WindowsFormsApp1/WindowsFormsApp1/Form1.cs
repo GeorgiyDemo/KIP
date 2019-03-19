44dd243c2cbc0450c17bdfd219c195508be7890a
Georgiy Demenchuk <demenchuk.george@mail.ru>
2019-03-19 14:20:51 +0300

    Пример приложения с ПМ.03

---
+++ session/Экзамены по модулям IV курс/ПМ.03/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void фИОBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.фИОBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.aNDREY);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "aNDREY.ФИО". При необходимости она может быть перемещена или удалена.
+            this.фИОTableAdapter.Fill(this.aNDREY.ФИО);
+
+        }
+    }
+}
---
+++ session/Экзамены по модулям IV курс/ПМ.03/WindowsFormsApp1/WindowsFormsApp1/TokenClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public static class TokenClass
+    {
+        public static readonly string SQLitepatch = "../../../../SMSTimetable.db";
+        public static readonly string MySQLConnectionString = "server=46.101.17.171;user=KOMUK;database=SMSSender_schema;password=meow1337;SslMode=none";
+
+    }
+}
